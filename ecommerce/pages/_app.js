d4df18683ba37d1cb51595f0801536f935c95090
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 11:05:57 +0200

    import react

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import React from 'react'; // To import to use .jsx
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const Home = () => {
   return (
     <>
