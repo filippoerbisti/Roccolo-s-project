419a42b7ffe902406a0e8ed4e9c966f07867906f
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 15:27:46 +0200

    add toaster

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react'; // To import to use .jsx
+import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
