5bb45b07c62367c45c37e53ddff5ebe32894f4ca
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:08:30 +0200

    map product

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -14,10 +14,10 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
 
     </div>
   )
