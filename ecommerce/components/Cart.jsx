f4afd5ba1b8ae2bafbcc4f08ef1b8ea64d215733
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 17:49:59 +0200

    add payment with stripe

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -6,10 +6,31 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return ;
+
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -85,7 +106,7 @@ const Cart = () => {
               <button 
                 type='button' 
                 className='btn'
-                onClick=""
+                onClick={handleCheckout}
               >
                 Pay with Stripe
               </button>
---
+++ ecommerce/pages/api/stripe.js
@@ -0,0 +1,46 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    //console.log(re.body.cartItems);
+
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode: 'payment',
+            payment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options: [
+                { shipping_rate: 'shr_1L5tXOGVmHSjXlXx8rsvS6iD' },
+                { shipping_rate: 'shr_1L5tYFGVmHSjXlXxEqRMQPQx' },
+            ],
+            line_items: req.body.cartItems.map((item) => {
+                const img = item.image[0].asset._ref;
+                const newImage = img.replace('image-', 'https://cdn.sanity.io/images/2ognk6zd/production/').replace('-webp', '.webp');
+
+                return {
+                    price_data: {
+                        currency: 'eur',
+                        product_data: {
+                            name: item.name,
+                            images: [newImage],
+                        },
+                        unit_amount: item.price * 100,
+                    }
+                }
+            }),
+            mode: 'payment',
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+        }
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create(params);
+        res.redirect(303, session.url);
+    } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+  }
+}
