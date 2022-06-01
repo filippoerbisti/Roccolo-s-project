8298694fb488042f4ebaf70df39d426306f14cc7
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:08:15 +0200

    wrap components

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react'; // To import to use .jsx
+
+import { Layout } from '../components';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
