d7583b1b75c061dcc228d966bf4bdacf81023b9d
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:08:42 +0200

    layout

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>WINE SIUM STORE</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
