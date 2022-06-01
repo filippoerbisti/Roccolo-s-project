086982a2bb4df49c62d1715493087657d6443d29
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:14:11 +0200

    footer

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 Eskere Headphones All Rights Reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
