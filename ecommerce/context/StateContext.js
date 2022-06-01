e4a99452b944950d94b5b0200a387599a5f4fb45
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 15:46:22 +0200

    set add and success toast

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -5,10 +5,33 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((utem) => cartItems._id === product._id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.quantity * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + product.quantities * quantity);
+
+        if(checkProductInCart) {
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if(cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+
+            setCartItems([...cartItems, { ...product }]);
+        }
+        toast.success(`${qty} ${product.name} added to the cart.`);
+    }
 
     const incQty = (() => {
         setQty((prevQty) => prevQty + 1);
@@ -32,6 +55,7 @@ export const StateContext = ({ children }) => {
                 qty,
                 incQty,
                 decQty,
+                onAdd,
             }}
         >
             {children}
