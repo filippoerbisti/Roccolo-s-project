7f76a6aa2cdeedff8ee240aae2b0fdf49621f8c9
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 15:11:50 +0200

    fixing

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
@@ -24,7 +25,7 @@ const ProductDetails = ({ product, products }) => {
                     </div> */}
                 </div>
 
-                <div className='product-details-desc'>
+                <div className='product-detail-desc'>
                     <h1>{name}</h1>
                     <div className='reviews'>
                         <div>
@@ -57,10 +58,30 @@ const ProductDetails = ({ product, products }) => {
                         <button 
                             type="button" 
                             className='add-to-cart' 
-                            onClick=""
+                            // onClick=""
                         >
                             Add to Cart
                         </button>
+                        <button 
+                            type="button" 
+                            className='buy-now' 
+                            // onClick=""
+                        >
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+
+            <div className='maylike-products-wrapper'>
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div className='maylike-products-container track'>
+                        {products.map((item) => (
+                            <Product key={item._id} 
+                            product={item} />
+                        ))}
                     </div>
                 </div>
             </div>
@@ -69,7 +90,11 @@ const ProductDetails = ({ product, products }) => {
 }
 
 export const getStaticPaths = async () => {
-    const query = '*[_type == "product"] { slug { current } }';
+    const query = `*[_type == "product"] {
+        slug { 
+             current 
+        } 
+    }`;
 
     const products = await client.fetch(query);
 
@@ -87,7 +112,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-    const productsQuery = '*[type == "product"]';
+    const productsQuery = '*[_type == "product"]';
 
     const product = await client.fetch(query);
     const products = await client.fetch(productsQuery);
