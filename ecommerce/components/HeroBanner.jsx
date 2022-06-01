7136b0b47b8626f5879becc35dcc5162e8ab8812
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 12:31:32 +0200

    dynamic data

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src='' alt='headphones' className='hero-banner-image' />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
 
         <div>
-          <Link href="/product/ID">
-            <button type='button'>BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
 
