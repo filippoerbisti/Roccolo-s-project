ece1b3187b9d66c2c174612f90ae7816f7870e77
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 15:52:16 +0200

    set show cart

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
+
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -12,11 +17,13 @@ const Navbar = () => {
       <button 
         type='button' 
         className='cart-icon'
-        onClick=""
+        onClick={() => setShowCart(true)}
       >
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -7,7 +7,7 @@ export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     const onAdd = (product, quantity) => {
@@ -49,6 +49,7 @@ export const StateContext = ({ children }) => {
         <Context.Provider
             value={{
                 showCart,
+                setShowCart,
                 cartItems,
                 totalPrice,
                 totalQuantities,
