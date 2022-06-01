1178c39de44a602288be70d75e22b24ddf48ff09
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:07:37 +0200

    footer

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const FooterBanner = () => {
+import { urlFor } from '../lib/client';
+
+const FooterBanner = ({ 
+    footerBanner: {
+      discount,
+      smallText,
+      midText,
+      largeText1,
+      largeText2,
+      saleTime,
+      product,
+      desc,
+      image,
+      buttonText
+    }
+  }) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className='right'>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+
+        <img src={urlFor(image)} alt="" className='footer-banner-image' />
+      </div>
+    </div>
   )
 }
 
