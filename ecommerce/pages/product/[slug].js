918c7982f13e40a6774221f096fad902c8c68adc
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 15:19:13 +0200

    detail

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
@@ -6,23 +6,24 @@ import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
 
     return (
         <div>
             <div className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])} className="product-detail-image" />
                     </div>
-                    {/* <div className='small-images-container'>
+                    <div className='small-images-container'>
                         {image?.map((item,i) => (
                             <img
                                 src={urlFor(item)}
-                                className=""
-                                onMouseEnter=""
+                                className={i === index ? 'small-image selected-image' : 'small-image'}
+                                onMouseEnter={() => setIndex(i)}
                             />
                         ))}
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className='product-detail-desc'>
