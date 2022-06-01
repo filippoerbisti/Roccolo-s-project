cf7fb6567907357a4544032b9a66cf4de5fb47bc
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:14:16 +0200

    navbar

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">ESKERE HEADPHONES</Link>
+      </p>
+
+      <button 
+        type='button' 
+        className='cart-icon'
+        onClick=""
+      >
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
