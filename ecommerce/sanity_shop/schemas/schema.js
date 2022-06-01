3775e1dd8b9716db6f0a962a3be1489193644bb8
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 10:32:04 +0200

    create schemas

--- ecommerce/sanity_shop/schemas/schema.js
+++ ecommerce/sanity_shop/schemas/schema.js
@@ -1,8 +1,12 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// Import my schema
+import product from './product';
+import banner from './banner';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +15,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    product,
+    banner
   ]),
 })
