3049f94575739024cc01dc2f401f7b566aca4758
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 14:07:47 +0200

    product

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Product = () => {
+import { urlFor } from '../lib/client';
+
+const Product = ({ 
+    product: {
+      image,
+      name,
+      slug,
+      price
+    } 
+  }) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img 
+            src={urlFor(image && image[0])} 
+            alt="" 
+            width={250}
+            height={250}
+            className='product-image'
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
