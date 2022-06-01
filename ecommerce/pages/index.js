d7d345e6df67fd4d099dbf9e44c477b24394b972
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-01 10:47:38 +0200

    homr

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,11 +1,24 @@
 import styles from '../styles/Home.module.css'
 
-import React from 'react'
-
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div>
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+
+      Footer
+
+    </>
   )
 }
 
-export default index
+export default Home
