566adbcac357876ee84d7df258ee4df8f8b43fb4
filippoerbisti <filippo.erbisti@gmail.com>
2022-06-02 12:00:24 +0200

    add func fireworks

--- ecommerce/pages/success.js
+++ ecommerce/pages/success.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 
 import { useStateContext } from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
 
 const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
@@ -12,7 +13,8 @@ const Success = () => {
         setCartItems([]);
         setTotalPrice(0);
         setTotalQuantities(0);
-    }, [] );
+        runFireworks();
+    }, []);
 
     return (
         <div className='success-wrapper'>
