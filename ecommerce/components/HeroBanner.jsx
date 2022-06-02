e63633957d77c059bfa237bb4c2e7c5b4bc3aac7
filippoerbisti <filippo.erbisti@gmail.com>
2022-06-02 11:33:23 +0200

    fix err

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -13,7 +13,7 @@ const HeroBanner = ({ heroBanner }) => {
         <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
 
         <div>
-          <Link href={`/product/€{heroBanner.product}`}>
+          <Link href={`/product/${heroBanner.product}`}>
             <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
