96fa71cd8dcd667b1f47e7e3ba96937dd95cdfa3
filippoerbisti <filippo.erbisti@gmail.com>
2022-06-04 14:14:33 +0200

    update footer

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillInstagram, AiOutlineFacebook } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 Eskere Headphones All Rights Reserved</p>
-      <p className='icons'>
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+      <p>2022 Roccolo del Garda © All Rights Reserved</p>
+      <p className='icons cursor-pointer'>
+        <a href="https://it-it.facebook.com/roccolodellago/" className='cursor-pointer' target="_blank">
+          <AiOutlineFacebook />
+        </a>
+        <a href="https://www.instagram.com/roccolodellago/" className='cursor-pointer' target="_blank">
+          <AiFillInstagram />
+        </a>
       </p>
     </div>
   )
