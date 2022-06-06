208bc338e4f13aea25574f74998c5de4b1712022
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-06 12:51:33 +0200

    navbar

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { AiOutlineShopping } from 'react-icons/ai';
 import useTranslation from 'next-translate/useTranslation';
 import { IT, GB, FR, DE } from 'country-flag-icons/react/3x2';
@@ -8,9 +9,41 @@ import styles from '../styles/Navbar.module.css';
 import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
-const Navbar = props => {
+const Navbar = () => {
   const { t } = useTranslation('common');
   const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  // const router = useRouter();
+  // const [activeTabs, setActiveTabs] = useState(props.name);
+
+  // useEffect(() => {
+  //   switch (activeTabs) {
+  //     case 'home':
+  //       router.push('/')
+  //       break;
+  //     case 'wine':
+  //       router.push('/wine')
+  //       break;
+  //     case 'farm':
+  //       router.push('/farm')
+  //       break;
+  //     case 'wineshop':
+  //       router.push('/wineshop')
+  //       break;
+  //     case 'event':
+  //       router.push('/event')
+  //       break;
+  //     case 'gallery':
+  //       router.push('/gallery')
+  //       break;
+  //     case 'about':
+  //       router.push('/about')
+  //       break;
+  //     default:
+  //       router.push('/')
+  //       break;
+  //   }
+  // }, [activeTabs, router]);
 
   return (
     <div className='navbar-container'>
@@ -25,13 +58,41 @@ const Navbar = props => {
 
       <ul className={styles.menu}>
         <li>
-          <Link href={'/wine'}>{t('navShop')}</Link>
-          <Link href={'/farm'}>{t('navFarm')}</Link>
-          <Link href={'/wineshop'}>{t('navWineShop')}</Link>
-          <Link href={'/event'}>{t('navEvents')}</Link>
-          <Link href={'/wine'}>{t('navWines')}</Link>
-          <Link href={'/gallery'}>{t('navGallery')}</Link>
-          <Link href={'/about'}>{t('navAbout')}</Link>
+          <Link href='/wine'>
+            <a className={activeTabs === 'wine' ? `${styles.active}` : ''} onClick={() => setActiveTabs('wine')}>
+              {t('navShop')}
+            </a>
+          </Link>
+          <Link href='/farm'>
+            <a className={activeTabs === 'farm' ? `${styles.active}` : ''} onClick={() => setActiveTabs('farm')}>
+              {t('navFarm')}
+            </a>
+          </Link>
+          <Link href='/wineshop'>
+            <a className={activeTabs === 'wineshop' ? `${styles.active}` : ''} onClick={() => setActiveTabs('wineshop')}>
+              {t('navWineShop')}
+            </a>
+          </Link>
+          <Link href='/event'>
+            <a className={activeTabs === 'event' ? `${styles.active}` : ''} onClick={() => setActiveTabs('event')}>
+              {t('navEvents')}
+            </a>
+          </Link>
+          <Link href='/wine'>
+            <a className={activeTabs === 'wine' ? `${styles.active}` : ''} onClick={() => setActiveTabs('wine')}>
+              {t('navWines')}
+            </a>
+          </Link>
+          <Link href='/gallery'>
+            <a className={activeTabs === 'gallery' ? `${styles.active}` : ''} onClick={() => setActiveTabs('gallery')}>
+              {t('navGallery')}
+            </a>
+          </Link>
+          <Link href='/about'>
+            <a className={activeTabs === 'about' ? `${styles.active}` : ''} onClick={() => setActiveTabs('about')}>
+              {t('navAbout')}
+            </a>
+          </Link>
         </li>
       </ul>
 
