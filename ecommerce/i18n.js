d62d1918f5ef30fb3fadda83340058563d4b4052
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-06 12:50:12 +0200

    i18n

--- ecommerce/i18n.js
+++ ecommerce/i18n.js
@@ -1,7 +1,21 @@
+// 2 equal ways:
+
+// Creating i18n.js (exports with module)
+
 module.exports = {
     locales: ['en', 'it', 'de', 'fr'], // Array with the languages that you want to use
     defaultLocale: 'en', // Default language of your website
     pages: {
-      '*': ['common'], // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
+      '*': ['common'], // Namespaces that you want to import per page
     },
   };
+
+
+// Creating i18n.json
+// {
+//   "locales": ["en", "it", "de", "fr"], // Array with the languages that you want to use
+//   defaultLocale: "en", // Default language of your website
+//   pages: {
+//     "*": ["common"] // Namespaces that you want to import per page
+//   }
+// };
