1aa85eeee2793b4f7a272c0507c8042974c958c7
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-06 12:50:40 +0200

    comment

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -24,16 +24,16 @@ const Home = ({ products, bannerData }) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
+// export const getServerSideProps = async () => {
+//   const query = '*[_type == "product"]';
+//   const products = await client.fetch(query);
 
-  const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+//   const bannerQuery = '*[_type == "banner"]';
+//   const bannerData = await client.fetch(bannerQuery);
 
-  return {
-    props: { products, bannerData }
-  }
-}
+//   return {
+//     props: { products, bannerData }
+//   }
+// }
 
 export default Home
