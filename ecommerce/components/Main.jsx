a636e6f813b4985bb4ad01e90142318c95c928f9
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-07 17:19:44 +0200

    temp

--- ecommerce/components/Main.jsx
+++ ecommerce/components/Main.jsx
@@ -23,6 +23,9 @@ const Main = () => {
     { url: "https://picsum.photos/seed/a/1600/900" },
     { url: "https://picsum.photos/seed/b/1920/1080" },
     { url: "https://picsum.photos/seed/c/1366/768" },
+    { url: "https://picsum.photos/seed/a/1600/900" },
+    { url: "https://picsum.photos/seed/b/1920/1080" },
+    { url: "https://picsum.photos/seed/c/1366/768" },
   ];
 
   const img1 = "/../public/degustazioni.png";
