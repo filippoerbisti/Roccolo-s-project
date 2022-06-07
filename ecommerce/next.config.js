bb0e2a4e04a627270715b89110e5308a1e227d7b
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-07 09:30:19 +0200

    config slider w/ images

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -2,11 +2,16 @@
 
 const nextTranslate = require('next-translate');
 
+const withImages = require("next-images");
+const withTM = require("next-transpile-modules")(["@madzadev/image-slider"]);
+
+// module.exports = withImages(withTM());
+
 const nextConfig = nextTranslate({
   reactStrictMode: true,
 });
 
-module.exports = nextConfig
+module.exports = withImages(withTM(nextConfig));
 
 // Equal way, work
 // module.exports = nextTranslate({
