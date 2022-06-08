62de1f67cd741b68d0f3ae072596060900862189
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 13:01:42 +0200

    add styles

--- ecommerce/components/Tasting.jsx
+++ ecommerce/components/Tasting.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 
+import styles from '../styles/Event.module.css';
+
 const Tasting = () => {
   const { t } = useTranslation('event');
 
   const tTasting = t('event:tasting', { count: 150 }, { returnObjects: true });
 
   return (
-    <div>
-      <h1>{tTasting.title}</h1>
-      <p>{tTasting.p}</p>
+    <div className={styles.mx20}>
+      <h1 className={styles.title}>{tTasting.title}</h1>
+      <p className={styles.paragraph}>{tTasting.p}</p>
     </div>
   )
 }
--- ecommerce/components/Wedding.jsx
+++ ecommerce/components/Wedding.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 
+import styles from '../styles/Event.module.css';
+
 const Wedding = () => {
   const { t } = useTranslation('event');
 
   const tWedding = t('event:wedding', { count: 150 }, { returnObjects: true });
 
   return (
-    <div>
-      <h1>{tWedding.title}</h1>
-      <p>{tWedding.p}</p>
+    <div className={styles.mx20}>
+      <h1 className={styles.title}>{tWedding.title}</h1>
+      <p className={styles.paragraph}>{tWedding.p}</p>
     </div>
   )
 }
