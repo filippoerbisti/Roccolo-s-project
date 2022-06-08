822a36c7a9779e58925915315318dcf614c04261
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 10:46:13 +0200

    data img

---
+++ ecommerce/store/dataGallery.js
@@ -0,0 +1,33 @@
+const dataGallery = [
+    { css: 'https://images.pexels.com/photos/416430/pexels-photo-416430.jpeg', height: 150 },
+    { css: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/358574/pexels-photo-358574.jpeg', height: 300 },
+    { css: 'https://images.pexels.com/photos/1738986/pexels-photo-1738986.jpeg', height: 350 },
+    { css: 'https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg', height: 300 },
+    { css: 'https://images.pexels.com/photos/1005644/pexels-photo-1005644.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/227675/pexels-photo-227675.jpeg', height: 300 },
+    { css: 'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/327482/pexels-photo-327482.jpeg', height: 400 },
+    { css: 'https://images.pexels.com/photos/2736834/pexels-photo-2736834.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/249074/pexels-photo-249074.jpeg', height: 150 },
+    { css: 'https://images.pexels.com/photos/310452/pexels-photo-310452.jpeg', height: 450 },
+    { css: 'https://images.pexels.com/photos/380337/pexels-photo-380337.jpeg', height: 200 },
+
+    { css: 'https://images.pexels.com/photos/416431/pexels-photo-416431.jpeg', height: 450 },
+    { css: 'https://images.pexels.com/photos/1103971/pexels-photo-1103971.jpeg', height: 350 },
+    { css: 'https://images.pexels.com/photos/911733/pexels-photo-911733.jpeg', height: 300 },
+    { css: 'https://images.pexels.com/photos/358573/pexels-photo-358573.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/358575/pexels-photo-358575.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/96387/pexels-photo-96387.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/1005643/pexels-photo-1005643.jpeg', height: 350 },
+    { css: 'https://images.pexels.com/photos/227673/pexels-photo-227673.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/325183/pexels-photo-325183.jpeg', height: 350 },
+    { css: 'https://images.pexels.com/photos/327483/pexels-photo-327483.jpeg', height: 200 },
+    { css: 'https://images.pexels.com/photos/327481/pexels-photo-327481.jpeg', height: 400 },
+    { css: 'https://images.pexels.com/photos/327485/pexels-photo-327485.jpeg', height: 350 },
+    { css: 'https://images.pexels.com/photos/310453/pexels-photo-310453.jpeg', height: 150 },
+    { css: 'https://images.pexels.com/photos/380333/pexels-photo-380333.jpeg', height: 300 },
+  ];
+
+  export default dataGallery
