70b77b53386f2006161b1a9fe97ad0d91036f1d9
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 17:57:30 +0200

    temp

--- ecommerce/components/About.jsx
+++ ecommerce/components/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import useTranslation from 'next-translate/useTranslation';
+
+import styles from '../styles/Contact.module.css';
 
 const About = () => {
+  const { t } = useTranslation('contact');
+
+  const tOption = t('contact:objectEmail', { count: 150 }, { returnObjects: true });
+
   return (
-    <div>About</div>
+    <div className={styles.mx60}>
+      <h1 className={styles.title}>{t('title')}</h1>
+      <p className={styles.paragraph}>{t('info')}</p>
+      <div>
+        <input type="text" placeholder={t('firstName')} />
+        <input type="text" placeholder={t('lastName')} />
+        <input type="text" placeholder={t('email')} />
+        <button>{t('button')}</button>
+      </div>
+    </div>
   )
 }
 
