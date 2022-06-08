f9dcc062063b295365b72d46c33c612c9de017d0
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 17:57:11 +0200

    add style

--- ecommerce/pages/contact.js
+++ ecommerce/pages/contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import About from '../components/About';
 
+import styles from '../styles/Contact.module.css';
+
 const Contact = () => {
   return (
-    <div>
+    <div className={styles.mx20}>
         <About />
     </div>
   )
