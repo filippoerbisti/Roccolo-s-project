0b6a4ed276ed951e92cc8e14156189973075a189
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 10:16:23 +0200

    export

--- ecommerce/components/index.js
+++ ecommerce/components/index.js
@@ -1,10 +1,12 @@
-export {default as Footer } from './Footer';
 export {default as Layout } from './Layout';
 export {default as Navbar } from './Navbar';
+export {default as Main} from './Main';
+export {default as Cart } from './Cart';
+export {default as ScrollToTop} from './ScrollToTop';
+export {default as Footer } from './Footer';
+
+export {default as Galleries} from './Galleries';
 export {default as Product } from './Product';
+
 export {default as HeroBanner } from './HeroBanner';
 export {default as FooterBanner } from './FooterBanner';
-export {default as Cart } from './Cart';
-export {default as BottomNav} from './BottomNav';
-export {default as ScrollToTop} from './ScrollToTop';
-export {default as Main} from './Main';
