df3b3a7fee9ef5b66e026f769afd340ffb8b8bf4
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 10:50:00 +0200

    fix

--- ecommerce/components/index.js
+++ ecommerce/components/index.js
@@ -8,5 +8,5 @@ export {default as Footer } from './Footer';
 export {default as Galleries } from './Galleries';
 export {default as Product } from './Product';
 
-export {default as HeroBanner } from './HeroBanner';
-export {default as FooterBanner } from './FooterBanner';
+export {default as HeroBanner } from './old/HeroBanner';
+export {default as FooterBanner } from './old/FooterBanner';
