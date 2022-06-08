78110dc038dc297c59e6cd71f155e83d4dd85951
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 14:25:32 +0200

    add styles

--- ecommerce/pages/event.js
+++ ecommerce/pages/event.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Tasting from '../components/Tasting';
 import Wedding from '../components/Wedding';
 
+import styles from '../styles/Event.module.css';
+
 const Event = () => {
   return (
-    <div>
-      <Tasting />
-      <Wedding />
+    <div className={styles.mx20}>
+      <Tasting id="tasting" />
+
+      <hr className={styles.hr} />
+
+      <Wedding id="wedding" />
     </div>
   )
 }
