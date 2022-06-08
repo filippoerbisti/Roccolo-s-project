8e025b71bfbc3729ae4328090329aef6d6478d5f
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-08 10:47:19 +0200

    add dataGallery

--- ecommerce/components/Galleries.jsx
+++ ecommerce/components/Galleries.jsx
@@ -5,6 +5,7 @@ import shuffle from 'lodash.shuffle';
 import useTranslation from 'next-translate/useTranslation';
 
 import styles from '../styles/Gallery.module.css';
+import dataGallery from '../store/dataGallery';
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
@@ -32,22 +33,7 @@ const useWindowSize = () => {
 const Galleries = () => {
   const { t } = useTranslation('common');
 
-  const data = [
-    { css: 'https://images.pexels.com/photos/416430/pexels-photo-416430.jpeg', height: 150 },
-    { css: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/358574/pexels-photo-358574.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/1738986/pexels-photo-1738986.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/1005644/pexels-photo-1005644.jpeg', height: 200 },
-    { css: 'https://images.pexels.com/photos/227675/pexels-photo-227675.jpeg', height: 300 },
-    { css: 'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg', height: 200 },
-    { css: 'https://images.pexels.com/photos/327482/pexels-photo-327482.jpeg', height: 400 },
-    { css: 'https://images.pexels.com/photos/2736834/pexels-photo-2736834.jpeg', height: 200 },
-    { css: 'https://images.pexels.com/photos/249074/pexels-photo-249074.jpeg', height: 150 },
-    { css: 'https://images.pexels.com/photos/310452/pexels-photo-310452.jpeg', height: 400 },
-    { css: 'https://images.pexels.com/photos/380337/pexels-photo-380337.jpeg', height: 200 },
-  ];
+  const data = dataGallery;
 
   // Hook1: Number of columns
   const size = useWindowSize();
@@ -66,9 +52,9 @@ const Galleries = () => {
   // Hook3: Hold items
   const [items, set] = useState(data);
 
-  // Hook4: shuffle data every 5 seconds
+  // Hook4: shuffle data every 10 seconds
   useEffect(() => {
-    const t = setInterval(() => set(shuffle), 5000)
+    const t = setInterval(() => set(shuffle), 10000)
     return () => clearInterval(t)
   }, []);
 
