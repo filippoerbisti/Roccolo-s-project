aa401e0e70d8236a0cb8bca4e5e35acfaa22a242
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-09 17:40:07 +0200

    title w/ translate

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
+import useTranslation from 'next-translate/useTranslation';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
 import ScrollToTop from './ScrollToTop';
 
 const Layout = ({ children }) => {
+  const { t } = useTranslation('common');
+
   return (
     <div>
       <Head>
-        <title>ROCCOLO DEL GARDA WINE SHOP</title>
+        <title>Roccolo del Lago - {t('navContact')}</title>
       </Head>
       <div>
       <header>
