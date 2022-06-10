bf59a7c915e65a9af3371ac0748a89bfa2584444
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-10 17:42:46 +0200

    set logic

--- ecommerce/components/ContactUs.jsx
+++ ecommerce/components/ContactUs.jsx
@@ -1,44 +1,128 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
+import axios from 'axios';
 
 import styles from '../styles/ContactUs.module.css';
 
 const ContactUs = () => {
   const { t } = useTranslation('contact');
 
-  const tOption = t('contact:objectEmail', { count: 150 }, { returnObjects: true });
-  const [optionValue, setOptionValue] = useState('');
-  const space = ' ';
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const fullname = firstname + " " + lastname;
 
-  function logValue() {
-    console.log(optionValue)
+  const tOption = t('contact:objectEmail', { count: 150 }, { returnObjects: true });
+
+  const handleSubmit = async (e) => { 
+    e.preventDefault();
+    console.log('Sending')
+
+    let data = {
+      fullname,
+      email,
+      subject,
+      message
+    }
+
+    axios.post('http://localhost:3000/api/mail', data, {
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(data)
+    })
+    // fetch('/api/mail', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json, text/plain, */*',
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(data)
+    // })
+    .then((res) => {
+      console.log('Response received')
+      if (res.status === 200) {
+        console.log('Response succeeded!')
+        setFirstname('');
+        setLastname('');
+        setEmail('');
+        setSubject(`${tOption.option0}`);
+        setMessage('');
+      }
+    })
+    .catch(
+      (e) => console.log(e),
+    );
   }
 
   return (
-    <div className={styles.mx60aic}>
+    <form method="post" onSubmit={handleSubmit} className={styles.mx60aic}>
       <h1 className={styles.title}>{t('title')}</h1>
       <p className={styles.paragraph}>{t('info')}</p>
       <div className={styles.center}>
         <div className={styles.row}>
           <div className={styles.col}>
             <label htmlFor={t('firstName')} className={styles.label}>{t('firstName')}</label>
-            <input type="text" required placeholder={t('firstName')} className={styles.input} name={t('firstName')} id={t('firstName')} />
+            <input 
+              type="text" 
+              required 
+              value={firstname}
+              onChange={(e) => {
+                setFirstname(e.target.value);
+              }}
+              placeholder={t('firstName')} 
+              className={styles.input} 
+              name={t('firstName')} 
+              id={t('firstName')} 
+            />
           </div>
           <div className={styles.col}>
             <label htmlFor={t('lastName')} className={styles.label}>{t('lastName')}</label>
-            <input type="text" required placeholder={t('lastName')} className={styles.input} name={t('lastName')} id={t('lastName')} />
+            <input 
+              type="text" 
+              required 
+              value={lastname}
+              onChange={(e) => {
+                setLastname(e.target.value);
+              }}
+              placeholder={t('lastName')} 
+              className={styles.input} 
+              name={t('lastName')} 
+              id={t('lastName')} 
+            />
           </div>
         </div>
 
         <div className={styles.col}>
           <label htmlFor={t('email')} className={styles.label}>{t('email')}</label>
-          <input type="email" required placeholder={t('email')} className={styles.input} name={t('email')} id={t('email')} />
+          <input 
+            type="email" 
+            required 
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder={t('email')} 
+            className={styles.input} 
+            name={t('email')} 
+            id={t('email')} 
+          />
         </div>
 
         <div className={styles.col}>
           <label htmlFor={t('object')} className={styles.label}>{t('object')}</label>
-          <select defaultValue={tOption.option0} onChange={(e) => {setOptionValue(e.target.value)}} className={styles.select} name={t('object')} id={t('object')}>
+          <select 
+            defaultValue={tOption.option0} 
+            onChange={(e) => {
+              setSubject(e.target.value)
+            }} 
+            className={styles.select} name={t('object')} 
+            id={t('object')}
+          >
             <option disabled>{tOption.option0}</option>
             <option value={tOption.option1}>{tOption.option1}</option>
             <option value={tOption.option2}>{tOption.option2}</option>
@@ -48,7 +132,19 @@ const ContactUs = () => {
 
         <div className={styles.col}>
           <label htmlFor={t('message')} className={styles.label}>{t('message')}</label>
-          <textarea required placeholder={t('message')} className={styles.textarea} name={t('message')} id={t('message')} cols="30" rows="10"></textarea>
+          <textarea 
+            required
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }} 
+            placeholder={t('message')} 
+            className={styles.textarea} 
+            name={t('message')} 
+            id={t('message')} 
+            cols="30" 
+            rows="10"
+          ></textarea>
         </div>
 
         <div className={styles.col}>
@@ -75,10 +171,10 @@ const ContactUs = () => {
         </div>
 
         <div className={styles.btnContainer}>
-          <button onClick={logValue} className={styles.btn}>{t('button')}</button>
+          <button type="submit" onClick={(e)=>{handleSubmit(e)}} className={styles.btn}>{t('button')}</button>
         </div>
       </div>
-    </div>
+    </form>
   )
 }
 
