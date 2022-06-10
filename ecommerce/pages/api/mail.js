5415bae4fc4e0f29f4c4cae74876e33abfd3a032
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-10 17:44:01 +0200

    implement

---
+++ ecommerce/pages/api/mail.js
@@ -0,0 +1,30 @@
+import { SMTPClient } from 'emailjs';
+ 
+export default function handler(req, res) {
+  const { fullname, email, subject, message } = req.body;
+
+  const client = new SMTPClient({
+    user: process.env.EMAIL,
+    password: process.env.PASSWORD,
+    host: 'smtp.gmail.com',
+    ssl: true,
+    port: 465,
+  });
+ 
+  try {
+    const send = client.sendAsync(
+      {
+        from: email,
+        to: process.env.EMAIL,
+        subject: `${subject}`,
+        text: `${fullname} wrote: ${message}`,
+      }
+    )
+  }
+  catch(e) {
+    res.status(400).end(JSON.stringify({ message:'ok' }))
+    return;
+  }
+ 
+  res.status(200).end(JSON.stringify({ message:'Send Mail' }))
+}
