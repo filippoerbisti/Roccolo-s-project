f463a7e923b155ae07dd0593eea92b65f1e1ef92
filippoerbisti <filippo.erbisti@gmail.com>
2022-06-13 00:33:59 +0200

    fix layout

--- ecommerce/pages/tour.js
+++ ecommerce/pages/tour.js
@@ -3,23 +3,53 @@ import useTranslation from 'next-translate/useTranslation';
 
 import React360Viewer from '../components/React360Viewer';
 // import '../styles/global.css';
-import styles from '../styles/Main.module.css'
+import styles from '../styles/Tour.module.css'
 
 const Tour = () => {
     const { t } = useTranslation('tour');
 
     return (
         <div className={styles.mx20}>
-                <div>
-                    <h1></h1>
-                    <React360Viewer
-                        amount={36}
-                        imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
-                        fileName="iris-{index}.jpeg"
-                        // spinReverse
-                        // autoplay
-                        buttonClass="light"
-                    />
+                <h1 className={styles.title}>VIRTUAL TOUR</h1>
+                <div className={styles.row}>
+                    <div className={styles.colmx20}>
+                        <h2 className={styles.title2}>VIRTUAL TOUR</h2>
+                        <p className={styles.paragraph}>ooooooooooo</p>
+                        <React360Viewer
+                            amount={36}
+                            imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
+                            fileName="iris-{index}.jpeg"
+                            // spinReverse
+                            // autoplay
+                            buttonClass="light"
+                        />
+                    </div>
+                    <div className={styles.colmx20}>
+                        <h2 className={styles.title2}>VIRTUAL TOUR</h2>
+                        <p className={styles.paragraph}>ooooooooooo</p>
+                        <React360Viewer
+                            amount={36}
+                            imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
+                            fileName="iris-{index}.jpeg"
+                            // spinReverse
+                            // autoplay
+                            buttonClass="light"
+                        />
+                    </div>
+                </div>
+                <div className={styles.w100}>
+                    <div className={styles.center}>
+                        <h2 className={styles.title2}>VIRTUAL TOUR</h2>
+                        <p className={styles.paragraph}>ooooooooooo</p>
+                        <React360Viewer
+                            amount={36}
+                            imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
+                            fileName="iris-{index}.jpeg"
+                            // spinReverse
+                            // autoplay
+                            buttonClass="light"
+                        />
+                    </div>
                 </div>
             </div>
     )
