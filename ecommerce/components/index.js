4dbf53a5154d834860ce890dcb8ffee8b03c7b37
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-14 15:28:26 +0200

    export

--- ecommerce/components/index.js
+++ ecommerce/components/index.js
@@ -5,10 +5,11 @@ export {default as Cart } from './Cart';
 export {default as ScrollToTop } from './ScrollToTop';
 export {default as Footer } from './Footer';
 
-export {default as Galleries } from './Galleries';
 export {default as Product } from './Product';
+export {default as WineShop } from './WineShop';
 export {default as Tasting } from './Tasting';
 export {default as Wedding } from './Wedding';
+export {default as Galleries } from './Galleries';
 export {default as ContactUs } from './ContactUs';
 
 export {default as HeroBanner } from './old/HeroBanner';
