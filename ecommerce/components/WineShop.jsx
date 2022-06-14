7888fc5060639f309cca85d4eaa8293b82274422
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-14 17:53:36 +0200

    temp

--- ecommerce/components/WineShop.jsx
+++ ecommerce/components/WineShop.jsx
@@ -9,24 +9,34 @@ import dataMainImgSlider from '../store/dataMainImgSlider';
 const WineShop = () => {
     const { t } = useTranslation('wineshop');
 
+    const pWineshop = `${t('paragraph')}`;
+    const pWineshopParagraph = pWineshop.replace(/xxx/gi, '\n\r');
+
     const images = dataMainImgSlider;
 
     return (
         <div className={styles.mx20}>
-            <h1 className={styles.title}>Wineshop</h1>
-            <p className={styles.paragraph}>paragraph</p>
-            <div className={styles.slider}>
-                <Slider 
-                imageList={images} 
-                width={1200} 
-                height={600} 
-                showArrowControls={true} 
-                showDotControls={true} 
-                loop={true}
-                autoPlay={true}
-                autoPlayInterval={5000}
-                bgColor="white"
-                />
+            <h1 className={styles.title}>{t('title')}</h1>
+            <h3 className={styles.title2}>{t('intro')}</h3>
+            <div className={styles.row}>
+                <div>
+                    <div className={styles.slider}>
+                        <Slider 
+                        imageList={images} 
+                        width={1200} 
+                        height={600} 
+                        showArrowControls={true} 
+                        showDotControls={true} 
+                        loop={true}
+                        autoPlay={true}
+                        autoPlayInterval={5000}
+                        bgColor="white"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <p className={styles.paragraph}>{pWineshopParagraph}</p>
+                </div>
             </div>
         </div>
     )
