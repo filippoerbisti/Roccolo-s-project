428ec6a43952627323f76c61fde4e15c8c04f14d
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-14 12:11:42 +0200

    change layout + clear

--- ecommerce/pages/tour.js
+++ ecommerce/pages/tour.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
 
 import React360Viewer from '../components/React360Viewer';
-// import '../styles/global.css';
 import styles from '../styles/Tour.module.css'
 
 const Tour = () => {
@@ -14,39 +14,29 @@ const Tour = () => {
             <p className={styles.paragr}>{t('paragraph')}</p>
             <div className={styles.row}>
                 <div className={styles.colmx20}>
-                    <h2 className={styles.title2}>{t('titleFarm')}</h2>
+                    <h2 className={styles.title2}>
+                        <Link href="/farm">{t('titleFarm')}</Link>
+                    </h2>
                     <p className={styles.paragraph}>{t('paragraphFarm')}</p>
                     <React360Viewer
                         amount={36}
                         imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
                         fileName="iris-{index}.jpeg"
-                        // spinReverse
+                        spinReverse
                         // autoplay
                         buttonClass="light"
                     />
                 </div>
                 <div className={styles.colmx20}>
-                    <h2 className={styles.title2}>{t('titleWineshop')}</h2>
+                    <h2 className={styles.title2}>
+                        <Link href="/wineshop">{t('titleWineshop')}</Link>
+                    </h2>
                     <p className={styles.paragraph}>{t('paragraphWineshop')}</p>
                     <React360Viewer
                         amount={36}
                         imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
                         fileName="iris-{index}.jpeg"
-                        // spinReverse
-                        // autoplay
-                        buttonClass="light"
-                    />
-                </div>
-            </div>
-            <div className={styles.w100}>
-                <div className={styles.center}>
-                    <h2 className={styles.title2}>{t('titleWedding')}</h2>
-                    <p className={styles.paragraph}>{t('paragraphWedding')}</p>
-                    <React360Viewer
-                        amount={36}
-                        imagePath="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car"
-                        fileName="iris-{index}.jpeg"
-                        // spinReverse
+                        spinReverse
                         // autoplay
                         buttonClass="light"
                     />
