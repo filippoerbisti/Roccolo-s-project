5fbf412686720a66eac9734cec537b0cc9e67a17
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-14 15:31:03 +0200

    add styles

--- ecommerce/pages/wineshop.js
+++ ecommerce/pages/wineshop.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import WineShop from '../components/WineShop';
+
+import styles from '../styles/WineShop.module.css';
 
 const Wineshop = () => {
   return (
-    <div>
-      cccc
+    <div className={styles.mx20}>
+      <WineShop />
     </div>
   )
 }
