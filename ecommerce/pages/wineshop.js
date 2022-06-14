2ee56e6dec5542852a4222ae26f2ffa4bc12f0f5
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-14 17:53:22 +0200

    get prodcuts + marquee

--- ecommerce/pages/wineshop.js
+++ ecommerce/pages/wineshop.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import WineShop from '../components/WineShop';
+import { client } from '../lib/client';
+import { Product, WineShop } from '../components';
+import useTranslation from 'next-translate/useTranslation';
 
 import styles from '../styles/WineShop.module.css';
 
-const Wineshop = () => {
+const Wineshop = ({ products }) => {
+  const { t } = useTranslation('wineshop');
+
   return (
     <div className={styles.mx20}>
       <WineShop />
+
+      <div className='maylike-products-wrapper'>
+        <h2>{t('ourWines')}</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products.map((product) => (
+              <Product key={product._id} 
+                product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  return {
+    props: { products }
+  }
 }
 
 export default Wineshop
