a82bddc3e9b6abce718db3ecbc4ad176979c5c47
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-15 10:52:54 +0200

    wineshop gallery

---
+++ ecommerce/store/dataWineShopGallery.js
@@ -0,0 +1,10 @@
+const dataWineShopGallery = [
+    { css: 'https://images.pexels.com/photos/416430/pexels-photo-416430.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/358574/pexels-photo-358574.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/1738986/pexels-photo-1738986.jpeg', height: 250 },
+    { css: 'https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg', height: 250 },
+  ];
+
+  export default dataWineShopGallery
