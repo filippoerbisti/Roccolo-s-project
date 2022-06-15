f574d95ca6b288a6e8b6b649910a063162aa83a8
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-15 10:54:38 +0200

    replace Slider with WineShopGallery

--- ecommerce/components/WineShop.jsx
+++ ecommerce/components/WineShop.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
-import Slider from "@madzadev/image-slider";
-    import "@madzadev/image-slider/dist/index.css";
 
 import styles from '../styles/WineShop.module.css';
-import dataMainImgSlider from '../store/dataMainImgSlider';
+import WineShopGalleries from './WineShopGalleries';
 
 const WineShop = () => {
     const { t } = useTranslation('wineshop');
@@ -12,30 +10,16 @@ const WineShop = () => {
     const pWineshop = `${t('paragraph')}`;
     const pWineshopParagraph = pWineshop.replace(/xxx/gi, '\n\r');
 
-    const images = dataMainImgSlider;
-
     return (
         <div className={styles.mx20}>
             <h1 className={styles.title}>{t('title')}</h1>
             <h3 className={styles.title2}>{t('intro')}</h3>
-            <div className={styles.row}>
-                <div className={styles.left}>
+            <div className={styles.col}>
+                <div className={styles.w50}>
                     <p className={styles.paragraph}>{pWineshopParagraph}</p>
                 </div>
-                <div className={styles.right}>
-                    <div className={styles.slider}>
-                        <Slider 
-                        imageList={images} 
-                        width={1200} 
-                        height={600} 
-                        showArrowControls={true} 
-                        showDotControls={true} 
-                        loop={true}
-                        autoPlay={true}
-                        autoPlayInterval={5000}
-                        bgColor="white"
-                        />
-                    </div>
+                <div className={styles.w50}>
+                    <WineShopGalleries />
                 </div>
             </div>
         </div>
