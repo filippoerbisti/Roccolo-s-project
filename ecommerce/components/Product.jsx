8d6eb6a591a6b558a10c4837672bb0c4ace11b64
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-15 16:10:53 +0200

    add style

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
+import styles from '../styles/Product.module.css';
 import { urlFor } from '../lib/client';
 
 const Product = ({ 
@@ -14,16 +15,17 @@ const Product = ({
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
-        <div className='product-card'>
+        <div className={styles['product-card']}>
           <img 
             src={urlFor(image && image[0])} 
             alt="" 
             width={250}
             height={250}
-            className='product-image'
+            objectFit="cover"
+            className={styles['product-image']}
           />
-          <p className='product-name'>{name}</p>
-          <p className='product-price'>€{price}</p>
+          <p className={styles['product-name']}>{name}</p>
+          <p className={styles['product-price']}>€{price}</p>
         </div>
       </Link>
     </div>
