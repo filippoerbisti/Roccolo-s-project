8f09a06d230c3179e55919c63c59221b2ab35924
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-15 10:53:05 +0200

    in progress

--- ecommerce/sanity_shop/schemas/product.js
+++ ecommerce/sanity_shop/schemas/product.js
@@ -1,4 +1,4 @@
-// Create schema for product
+// Create schema for product (wines)
 
 export default {
     name: 'product',
@@ -34,9 +34,52 @@ export default {
             type: 'number',
         },
         {
+            title: 'Rating',
+            name: 'rating',
+            // title: 'Rating (0 - 10)',
+            type: 'number'
+        },
+        {
             name: 'details',
             title: 'Details',
             type: 'string',
         },
     ],
+    orderings: [
+        {
+            name: 'priceDesc',
+            title: 'Price, Desc',
+            by: [
+                {field: 'price', direction: 'desc'}
+            ]
+        },
+        {
+            name: 'priceAsc',
+            title: 'Price, Asc',
+            by: [
+                {field: 'price', direction: 'asc'}
+            ]
+        },
+        {
+            title: 'Rating',
+            name: 'rating',
+            by: [
+              {field: 'rating', direction: 'desc'}
+            ]
+        }
+    ],
+    // preview: {
+    //     select: {
+    //       title: 'Slug',
+    //       rating: 'rating'
+    //     },
+    //     prepare(product, viewOptions = {}) {
+    //       const title = viewOptions.ordering && viewOptions.ordering.name === 'rating'
+    //       ? `${product.title} (${product.rating})`
+    //       : product.title
+    //       console.log(title)
+    
+    //       return {title: title}
+    //     }
+    // }
 }
