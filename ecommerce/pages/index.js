4794e08ddcf3da06aa824c7ebe659122b2990681
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-06-17 12:52:34 +0200

    catch ip

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { client } from '../lib/client';
 import Main from '../components/Main';
 
-const Home = ({ products }) => {
+const Home = ({ ip, products }) => {
+
+  console.log(ip)
 
   return (
     <div>
       <Main />
+
       {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]} /> */}
 
       {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
@@ -15,12 +18,24 @@ const Home = ({ products }) => {
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async ({ req }) => {
+
+  let ip;
+  if (req.headers["x-forwarded-for"]) {
+    ip = req.headers["x-forwarded-for"].split(',')[0]
+  } else if (req.headers["x-real-ip"]) {
+    ip = req.connection.remoteAddress
+  } else {
+    ip = req.connection.remoteAddress
+  }
+
+  // const ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
   return {
-    props: { products }
+    props: { ip, products }
   }
 }
 
