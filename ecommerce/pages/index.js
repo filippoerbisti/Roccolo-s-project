afad9c3df8ae390bab5ce155785a2474a6390fc3
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-08 15:58:11 +0200

    clear

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { client } from '../lib/client';
 import Main from '../components/Main';
-import DeviceDetector from "device-detector-js";
 
-const Home = ({ ip, products }) => {
-
-  const deviceDetector = new DeviceDetector();
-  const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
-  const device = deviceDetector.parse(userAgent);
-
-  console.dir('Browser:' + device.client.name, 'Device Type:' + device.device.type, 'Device Brand:' + device.device.brand, 'Device Model:' + device.device.model, 'Ip:' + ip);
+const Home = ({ products }) => {
 
   return (
     <div>
@@ -25,20 +18,11 @@ const Home = ({ ip, products }) => {
 
 export const getServerSideProps = async ({ req }) => {
 
-  let ip;
-  if (req.headers["x-forwarded-for"]) {
-    ip = req.headers["x-forwarded-for"].split(',')[0]
-  } else if (req.headers["x-real-ip"]) {
-    ip = req.connection.remoteAddress
-  } else {
-    ip = req.connection.remoteAddress
-  }
-
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
   return {
-    props: { ip, products }
+    props: { products }
   }
 }
 
