d69c4a74c2b43048f5a210a6984c3f4df95c5c24
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-08 11:08:06 +0200

    test featured background image Parallax Scrolling

---
+++ ecommerce/components/Moon.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/Moon.module.css';
+
+const Moon = () => {
+
+    if (typeof window !== 'undefined') {
+        let bg = document.getElementById('bg');
+        let moon = document.getElementById('moon');
+        let mountain = document.getElementById('mountain');
+        let road = document.getElementById('road');
+        let text = document.getElementById('text');
+
+        window.addEventListener('scroll', function() {
+            var value = this.window.scrollY;
+
+            bg.style.top = value * 0.5 + 'px';
+            moon.style.left = value * 0.5 + 'px';
+            mountain.style.top = value * 0.15 + 'px';
+            road.style.top = value * 0.15 + 'px';
+            text.style.top = value * 1 + 'px';
+        })
+    }
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.section}>
+                <Image src="/../public/moon/bg.jpg" id="bg" layout='fill' alt="" />
+                <Image src="/../public/moon/moon.png" id="moon" layout='fill' alt="" />
+                <Image src="/../public/moon/mountain.png" id="mountain" layout='fill' alt="" />
+                <Image src="/../public/moon/road.png" id="road" className={styles.road} layout='fill' alt="" />
+                <h2 className={styles.text} id="text">Moon Light</h2>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, eaque, minus error iste optio maiores ab vero, maxime similique reprehenderit et repellat asperiores quos in aspernatur doloribus sed? Dolorem, ut.</p>
+        </div>
+    )
+}
+
+export default Moon
---
+++ ecommerce/pages/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Moon from '../components/Moon';
+
+import styles from '../styles/Moon.module.css';
+
+const Event = () => {
+  return (
+    <div className={styles.mx20}>
+      <Moon />
+    </div>
+  )
+}
+
+export default Event
