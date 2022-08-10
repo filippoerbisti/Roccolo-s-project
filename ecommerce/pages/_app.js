5feed34ee2f7f9665e068a8611b41e92c9baa264
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-10 10:21:09 +0200

    add google analytics

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -69,6 +69,24 @@ function MyApp({ Component, pageProps }) {
         )}
       </Layout>
 
+      {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* Font Awesome */}
       <Script src="https://kit.fontawesome.com/60aa6b5946.js" crossOrigin="anonymous"></Script>
 
     </StateContext>
