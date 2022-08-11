8f7ae89c2133ba27f2d912ffd0281ae52507a679
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-11 12:33:05 +0200

    add new state for hamburger icon

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -6,6 +6,7 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
+    const [changeIconHamburgerMenu, setChangeIconHamburgerMenu] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantities, setTotalQuantities] = useState(0);
@@ -83,6 +84,8 @@ export const StateContext = ({ children }) => {
                 setShowCart, 
                 showMenu,
                 setShowMenu,
+                changeIconHamburgerMenu,
+                setChangeIconHamburgerMenu,
                 cartItems,
                 totalPrice,
                 totalQuantities,
