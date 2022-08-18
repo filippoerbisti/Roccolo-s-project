6d5aecbb0c701e67387138f8ba4fe0a7d67f9748
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-18 16:56:11 +0200

    clear

--- ecommerce/pages/privacy-policy.js
+++ ecommerce/pages/privacy-policy.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import useTranslation from 'next-translate/useTranslation';
 import PrivacyPolicyCookie from '../components/PrivacyPolicyCookie';
 
 import styles from '../styles/Policy.module.css';
 
 const PrivacyPolicy = () => {
-    const { t } = useTranslation('policy');
 
     return (
         <div className={styles.mx20}>
