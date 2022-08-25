4f40af3054c7f9019286970d7db874d725533ece
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-08-25 12:08:57 +0200

    tt

--- ecommerce/pages/tests.js
+++ ecommerce/pages/tests.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Look, View360 } from '../components';
+import { Look, Teste, View360 } from '../components';
 
 import styles from '../styles/Test.module.css';
 
 const Tests = () => {
   return (
     <div className={`${styles.mx20}`}>
-      <View360 className={styles.h} />
-      <Look />
+      <Teste />
+      {/* <View360 className={styles.h} />
+      <Look /> */}
     </div>
   )
 }
