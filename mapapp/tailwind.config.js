47544a31fd186ea0b38d391308323ab8aaaf3597
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-08 16:45:16 +0200

    Configure the template paths

--- mapapp/tailwind.config.js
+++ mapapp/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
