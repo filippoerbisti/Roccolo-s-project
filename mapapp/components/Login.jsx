14c6cc4a7a865247888b9492b9580f611a0e30ed
filippoerbisti <filippo.erbisti@gmail.com>
2022-09-10 20:31:05 +0200

    login form

--- mapapp/components/Login.jsx
+++ mapapp/components/Login.jsx
@@ -4,26 +4,27 @@ import Link from 'next/link';
 const Login = () => {
   return (
     <div className='login'>
-        <img 
-            src="https://res.cloudinary.com/dl38nyo08/image/upload/v1660806591/Roccolo%20del%20Lago/logo_iidjdd.png"
-            width={240} 
-            height={100} 
-            objectFit="contain"
-            className='cursor-pointer'
-        />
-        <form>
-          <h1>Please sign in</h1>
-
-          <div>
-            <input type="email" id="floatingInput" placeholder="name@example.com" />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div>
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <button type="submit">Sign in</button>
-        </form>
+        <div className='form-login'>
+            <img 
+                src="https://res.cloudinary.com/dl38nyo08/image/upload/v1660806591/Roccolo%20del%20Lago/logo_iidjdd.png"
+                width={240} 
+                height={100} 
+                objectFit="contain"
+                className='cursor-pointer'
+            />
+            <h1>INIZIA IL TOUR</h1>
+            <form>
+            <div class="form__group field">
+                    <input type="text" class="form__field" placeholder="Username" name="username" id='username' required />
+                    <label for="username" class="form__label">Username</label>
+                </div>
+                <div class="form__group field">
+                    <input type="password" class="form__field" placeholder="Password" name="password" id='password' required />
+                    <label for="password" class="form__label">Password</label>
+                </div>
+            <button type="submit">ACCEDI</button>
+            </form>
+        </div>
     </div>
   )
 }
