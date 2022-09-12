6a489454dc3061434a5113a1a49cba2d3a2e4ec2
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-12 13:00:22 +0200

    t

--- mapapp/components/Home.jsx
+++ mapapp/components/Home.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1 style="--count: 4;">
-            {/* <span style="--index: 0;">Slide</span>
-            <span style="--index: 1;">Slide</span>
-            <span style="--index: 2;">Slide</span>
-            <span style="--index: 3;">Slide</span>
-            <span>Slide</span> */}
-        </h1>
+        {/* <h1 style={{--count: 4;}}>
+            <span ariaHidden style={{--index: 0;}}>Slide</span>
+            <span ariaHidden style={{--index: 1;}}>Slide</span>
+            <span ariaHidden style={{--index: 2;}}>Slide</span>
+            <span ariaHidden style={{--index: 3;}}>Slide</span>
+            <span>Slide</span>
+        </h1> */}
     </div>
   )
 }
