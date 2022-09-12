4ef2f16f36ea584a9883b4b942f55c9cdcd71c18
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-12 16:41:04 +0200

    one title style css animated

--- mapapp/components/Home.jsx
+++ mapapp/components/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        {/* <h1 style={{--count: 4;}}>
-            <span ariaHidden style={{--index: 0;}}>Slide</span>
-            <span ariaHidden style={{--index: 1;}}>Slide</span>
-            <span ariaHidden style={{--index: 2;}}>Slide</span>
-            <span ariaHidden style={{--index: 3;}}>Slide</span>
+    <div className='slider-title'>
+        <h1 style={{"--count": 4}}>
+            <span aria-hidden style={{"--index": 0}}>Slide</span>
+            <span aria-hidden style={{"--index": 1}}>Slide</span>
+            <span aria-hidden style={{"--index": 2}}>Slide</span>
+            <span aria-hidden style={{"--index": 3}}>Slide</span>
             <span>Slide</span>
-        </h1> */}
+        </h1>
     </div>
   )
 }
