abe01eb7000e44d5545774240d01183af1e2e644
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-12 12:54:55 +0200

    home

---
+++ mapapp/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 style="--count: 4;">
+            {/* <span style="--index: 0;">Slide</span>
+            <span style="--index: 1;">Slide</span>
+            <span style="--index: 2;">Slide</span>
+            <span style="--index: 3;">Slide</span>
+            <span>Slide</span> */}
+        </h1>
+    </div>
+  )
+}
+
+export default Home
