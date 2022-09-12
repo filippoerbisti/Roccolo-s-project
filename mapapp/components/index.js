c0878b926ca87778a805c131c497f050eec1823b
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-12 12:54:10 +0200

    export

--- mapapp/components/index.js
+++ mapapp/components/index.js
@@ -1,7 +1,11 @@
 export {default as Layout } from './Layout';
 export {default as Login } from './Login';
 export {default as Navbar } from './Navbar';
+export {default as Main } from './Main';
 export {default as BottomBar } from './BottomBar';
 
-export {default as Main } from './Main';
+export {default as Home } from './Home';
+
 export {default as Mapping } from './Mapping';
+
+export {default as About } from './About';
