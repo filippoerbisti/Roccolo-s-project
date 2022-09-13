bbd1891ecf8686b07466979221581b6719ef731e
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-13 08:42:05 +0200

    add title

--- mapapp/components/HomeTitle.jsx
+++ mapapp/components/HomeTitle.jsx
@@ -4,11 +4,11 @@ const HomeTitle = () => {
   return (
     <div className='slider-title'>
         <h1 style={{"--count": 4}}>
-            <span aria-hidden style={{"--index": 0}}>Slide</span>
-            <span aria-hidden style={{"--index": 1}}>Slide</span>
-            <span aria-hidden style={{"--index": 2}}>Slide</span>
-            <span aria-hidden style={{"--index": 3}}>Slide</span>
-            <span>Slide</span>
+            <span aria-hidden style={{"--index": 0}}>Titolo</span>
+            <span aria-hidden style={{"--index": 1}}>Titolo</span>
+            <span aria-hidden style={{"--index": 2}}>Titolo</span>
+            <span aria-hidden style={{"--index": 3}}>Titolo</span>
+            <span>Titolo</span>
         </h1>
     </div>
   )
