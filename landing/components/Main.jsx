e42d3d501b96a55002a3379a316288fb73b7aea8
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-14 09:33:31 +0200

    add parallax effect

--- landing/components/Main.jsx
+++ landing/components/Main.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+        <section class="section-background parallax">
+            <h2>Desert</h2>
+        </section>
+        <section class="text-bloc">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis quibusdam, ex totam aliquam provident alias culpa, sit illo, eum doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore nemo veniam maiores vitae deserunt cum ducimus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis quibusdam, ex totam aliquam provident alias culpa, sit illo, eum doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore nemo veniam maiores vitae deserunt cum ducimus.</p>
+        </section>
+        <section class="section-background parallax1">
+            <h2>Mountain</h2>
+        </section>
+            
+        <section class="text-bloc">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis quibusdam, ex totam aliquam provident alias culpa, sit illo, eum doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore nemo veniam maiores vitae deserunt cum ducimus.</p>
+        </section>
+            
+        <section class="section-background parallax2">
+            <h2>Sea</h2>
+        </section>
+        <section class="section-background parallax3">
+            <h2>Galaxy</h2>
+        </section>
+    </div>
   )
 }
 
