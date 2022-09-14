51f5dcdc654696c0a7bd37de21ff4a340404bdcb
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-14 13:00:47 +0200

    scan

---
+++ mapapp/pages/scan.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ScanReader from "../components/ScanReader";
+
+function Scan() {
+
+  return (
+    <div>
+        <ScanReader />
+    </div>
+  );
+}
+
+export default Scan;
