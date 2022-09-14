da53671b7d71830283a1d3b660859aae8216efd0
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-14 09:08:45 +0200

    integrate iframe genially

--- mapapp/components/Mapping.jsx
+++ mapapp/components/Mapping.jsx
@@ -7,23 +7,30 @@ const IMG_URL =
 
 const Mapping = () => {
 
-    const imgRef = useRef();
-    const onUpdate = useCallback(({ x, y, scale }) => {
-      const { current: img } = imgRef;
-  
-      if (img) {
-        const value = make3dTransformValue({ x, y, scale });
-        img.style.setProperty("transform", value);
-      }
-    }, []);
+  const imgRef = useRef();
+  const onUpdate = useCallback(({ x, y, scale }) => {
+    const { current: iframe } = imgRef;
 
-    return (
-      <>
-        <QuickPinchZoom onUpdate={onUpdate}>
-          <img ref={imgRef} src={IMG_URL}/>
-        </QuickPinchZoom>
-      </>
-    )
+    if (iframe) {
+      const value = make3dTransformValue({ x, y, scale });
+      iframe.style.setProperty("transform", value);
+    }
+  }, []);
+
+  return (
+    <>
+      <QuickPinchZoom onUpdate={onUpdate}>
+        {/* <img ref={imgRef} src={IMG_URL}/> */}
+        <div style={{width: '100%'}} ref={imgRef}>
+          <div style={{position: "relative", paddingBottom: "74.9791492910759%", paddingTop: 0, "height": 0}}>
+            <iframe frameBorder="0" width="1199px" height="899px" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}} 
+            src="https://view.genial.ly/6320e70918827800110ca871" type="text/html" allowscriptaccess="always" allowFullScreen={true} scrolling="yes" allownetworking="all">
+            </iframe> 
+          </div> 
+        </div>
+      </QuickPinchZoom>
+    </>
+  )
 }
 
 export default Mapping
