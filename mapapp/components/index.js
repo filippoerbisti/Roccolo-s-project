e7c0345597ec1b2f88655e12d26b313635fb4f1a
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-14 17:07:07 +0200

    export

--- mapapp/components/index.js
+++ mapapp/components/index.js
@@ -2,11 +2,12 @@ export {default as Layout } from './Layout';
 export {default as Login } from './Login';
 export {default as Navbar } from './Navbar';
 export {default as Main } from './Main';
-export {default as QReader } from './QReader';
+export {default as QReaderIcon } from './QReaderIcon';
 export {default as ScanReader } from './ScanReader';
 export {default as BottomBar } from './BottomBar';
 
 export {default as HomeTitle } from './HomeTitle';
+export {default as Stage } from './Stage';
 
 export {default as Mapping } from './Mapping';
 
