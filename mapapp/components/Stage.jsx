30565191ad5532ebb0e9c84138d1214c9cb49eab
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-14 17:08:04 +0200

    st

---
+++ mapapp/components/Stage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Stage = () => {
+
+    return (
+        <>
+            {/* <!-- Trigger/Open The Modal --> */}
+            {/* <button onClick={modalShow}>
+                <QReaderIcon />
+            </button> */}
+            
+            {/* <!-- The Modal --> */}
+            {/* <div id="myModal" className="modal"> */}
+                {/* <!-- Modal content --> */}
+                {/* <div className="modal-content">
+                    <div className="modal-body">
+                        <span className="close" onClick={modalClose}>&times;</span>
+                        <ScanReader />
+                    </div>
+                </div>
+            </div> */}
+        </>
+    )
+}
+
+export default Stage
