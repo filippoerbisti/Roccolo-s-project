d0e620704f39ece74e869e0a89f1c2049c1a3869
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-15 11:45:33 +0200

    create loader

---
+++ mapapp/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className='walker'>
+            <div id="walk-container">
+                <div id="walk"></div>
+            </div>
+            <p>Caricamento ...</p>
+        </div>
+    )
+}
+
+export default Loader
