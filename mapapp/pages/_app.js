941fdeff72b6ae7e14ff008b351af8ed5e71d05d
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-15 11:45:20 +0200

    add loader

--- mapapp/pages/_app.js
+++ mapapp/pages/_app.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
-import { Layout, Navbar, BottomBar } from '../components';
+import { Layout, Navbar, BottomBar, Loader } from '../components';
 import '../styles/globals.css'
 import { AuthContextProvider } from '../context/AuthContext';
 import ProtectedRoute from '../components/ProtectedRoute';
@@ -12,18 +12,32 @@ const noAuthRequired = ['/', '/signup'];
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
+  const [isLoaded, setIsLoaded] = useState(false);    
+
+  useEffect(() => {
+    setTimeout(() => setIsLoaded(true), 1000)
+  }, [isLoaded]);
+
   return (
     <AuthContextProvider>
       
-      <Layout>
-        {noAuthRequired.includes(router.pathname) ? (
-          <Component {...pageProps} />
-        ) : (
-          <ProtectedRoute>
+      {isLoaded &&
+        <Layout>
+          {noAuthRequired.includes(router.pathname) ? (
             <Component {...pageProps} />
-          </ProtectedRoute>
-        )}
-      </Layout>
+          ) : (
+            <ProtectedRoute>
+              <Component {...pageProps} />
+            </ProtectedRoute>
+          )}
+        </Layout>
+      }
+
+      {!isLoaded && 
+        <Layout>
+          <Loader />
+        </Layout>
+      }
 
       {/* Font Awesome */}
       <Script src="https://kit.fontawesome.com/60aa6b5946.js" crossOrigin="anonymous"></Script>
