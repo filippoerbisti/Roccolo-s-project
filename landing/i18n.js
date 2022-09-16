2a4882f02a0c96cbbdef6b540fcbb70f3f483693
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-16 11:43:34 +0200

    fix warn in console i18nConfig

--- landing/i18n.js
+++ landing/i18n.js
@@ -1,9 +1,8 @@
 // Creating i18n.js (exports with module)
 
 const i18nConfig = {
-    locales: ['__default', 'en', 'it', 'de', 'fr'], // Array with the languages that you want to use
-    defaultLocale: '__default', // Default language of your website
-    localesToIgnore: ['__default'],
+    locales: ['default', 'en', 'it', 'de', 'fr'], // Array with the languages that you want to use
+    defaultLocale: 'default', // Default language of your website
     pages: {
       '*': ['common', 'meta'], // Namespaces that you want to import per page
     },
