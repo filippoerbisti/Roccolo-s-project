a092c446e9b20bc7570c7523e13aab0b41392ff4
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-16 13:00:00 +0200

    store data fake paths

---
+++ mapapp/store/dataFakePath.js
@@ -0,0 +1,41 @@
+const dataFakePath = [
+    { 
+        id: 1,
+        title: 'Punto Iniziale',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Punto iniziale - Start'
+    },
+    { 
+        id: 2,
+        title: 'Roccolo del Lago',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Roccolo: dove i cacciatori si riunivano'
+    },
+    { 
+        id: 3,
+        title: 'Olivo secolare',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Bellissimo olivo antico'
+    },
+    { 
+        id: 4,
+        title: 'Laghetto Naturale',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Lago naturale con fauna locale'
+    },
+    { 
+        id: 5,
+        title: 'Panoramica su vigneti',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Ammira i nostri vigneti'
+    },
+    { 
+        id: 6,
+        title: 'Cantina vinicola',
+        img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
+        description: 'Assaggia i nostri vini, compra'
+    }
+
+  ];
+
+  export default dataFakePath
