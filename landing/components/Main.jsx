446f92b35c1fc60db5fc1d08331b4f08286f348b
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-16 10:00:58 +0200

    chng txt

--- landing/components/Main.jsx
+++ landing/components/Main.jsx
@@ -4,13 +4,13 @@ const Main = () => {
   return (
     <div>
         <section className="section-background parallax">
-            <h2>Desert</h2>
+            <h2>Roccolo del Lago</h2>
         </section>
         <section className="text-bloc">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis quibusdam, ex totam aliquam provident alias culpa, sit illo, eum doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore nemo veniam maiores vitae deserunt cum ducimus.</p>
         </section>
         <section className="section-background parallax1">
-            <h2>Mountain</h2>
+            <h2>I nostri Vini</h2>
         </section>
             
         <section className="text-bloc">
@@ -20,10 +20,10 @@ const Main = () => {
         </section>
             
         <section className="section-background parallax2">
-            <h2>Sea</h2>
+            <h2>La nostra Uva</h2>
         </section>
         <section className="section-background parallax3">
-            <h2>Galaxy</h2>
+            <h2>I nostri Vigneti</h2>
         </section>
                 
     </div>
