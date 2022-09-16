556ade5ea019a4f9e1c40fe619d3e2cc58501c92
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-16 11:38:01 +0200

    config next-translate

---
+++ mapapp/i18n.js
@@ -0,0 +1,14 @@
+// Creating i18n.js (exports with module)
+
+const i18nConfig = {
+    locales: ['__default', 'en', 'it', 'de', 'fr'], // Array with the languages that you want to use
+    defaultLocale: '__default', // Default language of your website
+    localesToIgnore: ['__default'],
+    pages: {
+      '*': ['common', 'meta'], // Namespaces that you want to import per page
+      '/': ['home'],
+      '/success': ['success'],
+    },
+  };
+
+  module.exports = i18nConfig;
