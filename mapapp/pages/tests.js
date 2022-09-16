e5be24d59b4a1b7ac397443cfd1713e89e134dc0
filippoerbisti <filippo.erbisti@gmail.com>
2022-09-16 18:45:18 +0200

    test

--- mapapp/pages/tests.js
+++ mapapp/pages/tests.js
@@ -4,7 +4,7 @@ import { Home } from '../components'
 const Tests = () => {
   return (
     <div>
-        <Home />
+        {/* <div style="width: 100%;"><div style="position: relative; padding-bottom: 74.98%; padding-top: 0; height: 0;"><iframe frameBorder="0" width="1199" height="899" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/6320e70918827800110ca871" type="text/html" allowscriptaccess="always" allowFullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div> </div> */}
     </div>
   )
 }
