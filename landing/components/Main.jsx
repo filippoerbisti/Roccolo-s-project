15e9143a2b81bbdb0eb21b7e668d7347da93a8d3
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-16 11:18:36 +0200

    add transl

--- landing/components/Main.jsx
+++ landing/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import useTranslation from 'next-translate/useTranslation';
 import toast from 'react-hot-toast';
 
 const Main = () => {
+    const { t } = useTranslation('common');
     const router = useRouter();
     const mapapp = 'https://mapapproccolo.vercel.app';
 
@@ -17,18 +19,18 @@ const Main = () => {
                 <div>
                     <h2>Roccolo del Lago</h2>
                     <div className='btn-container'>
-                        <button onClick={bookTastingNow} className='btn'>Prenota Ora</button>
+                        <button onClick={bookTastingNow} className='btn'>{t('bookNow')}</button>
                     </div>
                 </div>
             </section>
             <section className="text-bloc">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis quibusdam, ex totam aliquam provident alias culpa, sit illo, eum doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore nemo veniam maiores vitae deserunt cum ducimus.</p>
                 <div className='btn-container'>
-                    <button onClick={bookTastingNow} className='btn'>Prenota Ora</button>
+                    <button onClick={bookTastingNow} className='btn'>{t('bookNow')}</button>
                 </div>
             </section>
             <section className="section-background parallax1">
-                <h2>I nostri Vini</h2>
+                <h2>{t('ourWines')}</h2>
             </section>
                 
             <section className="text-bloc">
@@ -38,10 +40,10 @@ const Main = () => {
             </section>
                 
             <section className="section-background parallax2">
-                <h2>La nostra Uva</h2>
+                <h2>{t('ourGrape')}</h2>
             </section>
             <section className="section-background parallax3">
-                <h2>I nostri Vigneti</h2>
+                <h2>{t('ourVineyards')}</h2>
             </section>
                     
         </div>
