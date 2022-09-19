d1556a46ee9b88b51a8d30891e46a1fed8362bc0
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-19 16:32:16 +0200

    add params to dataFakePath

--- mapapp/store/dataFakePath.js
+++ mapapp/store/dataFakePath.js
@@ -1,6 +1,7 @@
 const dataFakePath = [
     { 
         id: 1,
+        path: 'path1',
         title: 'Punto Iniziale',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', 
         description: 'Punto iniziale - Start',
@@ -8,6 +9,7 @@ const dataFakePath = [
     },
     { 
         id: 2,
+        path: 'path2',
         title: 'Roccolo del Lago',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png', 
         description: 'Roccolo: dove i cacciatori si riunivano',
@@ -15,6 +17,7 @@ const dataFakePath = [
     },
     { 
         id: 3,
+        path: 'path3',
         title: 'Olivo secolare',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394842/Roccolo%20del%20Lago/casual%20img/pexels-rachel-claire-4825743_rumetz.jpg', 
         description: 'Bellissimo olivo antico',
@@ -22,6 +25,7 @@ const dataFakePath = [
     },
     { 
         id: 4,
+        path: 'path4',
         title: 'Laghetto Naturale',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394250/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-51947_iuzicl.jpg', 
         description: 'Lago naturale con fauna locale',
@@ -29,6 +33,7 @@ const dataFakePath = [
     },
     { 
         id: 5,
+        path: 'path5',
         title: 'Panoramica su vigneti',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394258/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-39511_nkcwju.jpg', 
         description: 'Ammira i nostri vigneti',
@@ -36,6 +41,7 @@ const dataFakePath = [
     },
     { 
         id: 6,
+        path: 'path6',
         title: 'Cantina vinicola',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-434311_dbuhns.jpg', 
         description: 'Assaggia i nostri vini, compra',
