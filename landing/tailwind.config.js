02cb32813accdb42b7ecd98c1865bcaf26ec14bc
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-20 08:44:45 +0200

    add content tailwind.config

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ], // in our case we need only js, jsx
   theme: {
     extend: {},
   },
