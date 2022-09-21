e142c04cabb4a56324db97773c034c356c0387e7
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-21 15:11:41 +0200

    init sanity lib

---
+++ mapapp/lib/client.js
@@ -0,0 +1,16 @@
+// import sanityClient from '@sanity/client';
+// import imageUrlBuilder from '@sanity/image-url';
+
+// // To connect to Sanity.io
+// // https://www.sanity.io/manage/personal/project/2ognk6zd
+// export const client = sanityClient({
+//     projectId: '2ognk6zd',
+//     dataset: 'production',
+//     apiVersion: '2022-03-10',
+//     useCdn: true,
+//     token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+// });
+
+// const builder = imageUrlBuilder(client);
+
+// export const urlFor = (source) => builder.image(source);
