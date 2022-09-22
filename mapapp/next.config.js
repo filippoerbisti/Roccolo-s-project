716236da5faf0cff2071d1fa37d8a12b1f643afe
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-22 10:31:58 +0200

    config next

--- mapapp/next.config.js
+++ mapapp/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextTranslate = require('next-translate');
+const withPWA = require("next-pwa");
 
 const nextConfig = nextTranslate({
   reactStrictMode: true,
@@ -8,6 +9,11 @@ const nextConfig = nextTranslate({
     domains: ["res.cloudinary.com", "https://view.genial.ly/"],
   },
   swcMinify: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
 });
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
