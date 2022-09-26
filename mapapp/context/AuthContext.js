70db630e00654244c137971f3376d87350cf33c8
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-26 15:44:11 +0200

    get AuthorizedDate from firebase

--- mapapp/context/AuthContext.js
+++ mapapp/context/AuthContext.js
@@ -19,6 +19,7 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [authorizedDates, setAuthorizedDates] = useState(null);
   const [paths, setPaths] = useState(null);
 
   useEffect(() => {
@@ -30,10 +31,13 @@ export const AuthContextProvider = ({ children }) => {
           email: user.email
         });
         if(user.uid)
+        {
+          getAuthorizedDate();
           getPaths();
-        
+        }
       } else {
         setUser(null);
+        setAuthorizedDates(null);
         setPaths(null);
       }
       setLoading(false);
@@ -42,30 +46,57 @@ export const AuthContextProvider = ({ children }) => {
     return () => unsubscribe();
   });
 
-    const getPaths = () => {
-      if(user !== null) {
-        getDoc(doc(database, "user_paths", user.email)).then(docSnap => {
-          if (docSnap.exists())
-            setPaths(docSnap.data());
-          else {
-            setDoc(doc(database, "user_paths", user.email), {
-              id: user.uid,
-              path1: false,
-              path2: false,
-              path3: false,
-              path4: false,
-              path5: false,
-              path6: false,
-              userId: user.uid,
-              userMail: user.email
-            });
-            getDoc(doc(database, "user_paths", user.email)).then(docSnap => {
-              setPaths(docSnap.data());
-            });
-          }
-        })
-      }
+  const getAuthorizedDate = () => {
+    if(user !== null) {
+      getDoc(doc(database, "user_authorized_dates", user.email)).then(docSnap => {
+        if (docSnap.exists())
+          setAuthorizedDates(docSnap.data());
+
+          //TODO: create on registration
+        // else {
+        //   setDoc(doc(database, "user_authorized_dates", user.email), {
+        //     id: user.uid,
+        //     path1: false,
+        //     path2: false,
+        //     path3: false,
+        //     path4: false,
+        //     path5: false,
+        //     path6: false,
+        //     userId: user.uid,
+        //     userMail: user.email
+        //   });
+        //   getDoc(doc(database, "user_paths", user.email)).then(docSnap => {
+        //     setPaths(docSnap.data());
+        //   });
+        // }
+      })
     }
+  }
+
+  const getPaths = () => {
+    if(user !== null) {
+      getDoc(doc(database, "user_paths", user.email)).then(docSnap => {
+        if (docSnap.exists())
+          setPaths(docSnap.data());
+        else {
+          setDoc(doc(database, "user_paths", user.email), {
+            id: user.uid,
+            path1: false,
+            path2: false,
+            path3: false,
+            path4: false,
+            path5: false,
+            path6: false,
+            userId: user.uid,
+            userMail: user.email
+          });
+          getDoc(doc(database, "user_paths", user.email)).then(docSnap => {
+            setPaths(docSnap.data());
+          });
+        }
+      })
+    }
+  }
 
   const signup = (email, password) => {    
     return createUserWithEmailAndPassword(auth, email, password)
@@ -83,7 +114,7 @@ export const AuthContextProvider = ({ children }) => {
   }
 
   return (
-    <AuthContext.Provider value={{ user, paths, login, signup, logout }}>
+    <AuthContext.Provider value={{ user, authorizedDates, paths, login, signup, logout }}>
       {loading ? null : children}
     </AuthContext.Provider>
   )
