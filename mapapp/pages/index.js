03a1c4646c9ebae0df54d7283230ec491c8f77d1
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-26 18:08:27 +0200

    menage AuthorizedDates

--- mapapp/pages/index.js
+++ mapapp/pages/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Main } from '../components';
 import Login from '../components/Login';
+import NoAuthPeriod from '../components/NoAuthPeriod';
 
 import { useAuth } from '../context/AuthContext';
 
@@ -13,23 +14,32 @@ export default function Index() {
 
   useEffect(() => {
     if (authorizedDates) {
-      var start = new Date(authorizedDates.start_date.toDate());
-      var end = new Date(authorizedDates.end_date.toDate());
-      var today = new Date();
+      var start = new Date(authorizedDates.start_date.toDate()).getTime();
+      var end = new Date(authorizedDates.end_date.toDate()).getTime();
+      var today = new Date().getTime();
+
       if (today >= start && today <= end)
         setIsAuthPeriod(true);
     }
   }, [isAuthPeriod]);
-
+  
   return (
     <div>
+      {/* If not logged in -> show Login */}
       {!user &&
         <Login />
       }
 
+      {/* If logged in: */}
+      {/* If logged in && Date.Now is BETWEEN the fixed initial date (choose by user on pay) and the following 6 days (inclusive) */}
       {isAuthPeriod &&
         user && paths &&
           <Main paths={paths} />
+      }
+
+      {/* If logged in && Date.Now is NOT BETWEEN the fixed initial date (choose by user on pay) and the following 6 days (inclusive) */}
+      {!isAuthPeriod &&
+        <NoAuthPeriod />
       }
     </div>
   )
