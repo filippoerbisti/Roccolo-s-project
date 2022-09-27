36ca7100516980eafb44fa1238421e8440ca0a80
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-27 09:10:37 +0200

    fix authorizedDates

--- mapapp/pages/index.js
+++ mapapp/pages/index.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { Main } from '../components';
 import Login from '../components/Login';
-import NoAuthPeriod from '../components/NoAuthPeriod';
 
 import { useAuth } from '../context/AuthContext';
 
@@ -10,18 +9,6 @@ export default function Index() {
   const { user } = useAuth();
   const { authorizedDates } = useAuth();
   const { paths } = useAuth();
-  const [isAuthPeriod, setIsAuthPeriod] = useState(false);
-
-  useEffect(() => {
-    if (authorizedDates) {
-      var start = new Date(authorizedDates.start_date.toDate()).getTime();
-      var end = new Date(authorizedDates.end_date.toDate()).getTime();
-      var today = new Date().getTime();
-
-      if (today >= start && today <= end)
-        setIsAuthPeriod(true);
-    }
-  }, [isAuthPeriod]);
   
   return (
     <div>
@@ -30,16 +17,9 @@ export default function Index() {
         <Login />
       }
 
-      {/* If logged in: */}
-      {/* If logged in && Date.Now is BETWEEN the fixed initial date (choose by user on pay) and the following 6 days (inclusive) */}
-      {isAuthPeriod &&
-        user && paths &&
-          <Main paths={paths} />
-      }
-
-      {/* If logged in && Date.Now is NOT BETWEEN the fixed initial date (choose by user on pay) and the following 6 days (inclusive) */}
-      {!isAuthPeriod &&
-        <NoAuthPeriod />
+      {/* If logged in -> show main */}
+      {user && paths &&
+        <Main user={user} authorizedDates={authorizedDates} paths={paths} />
       }
     </div>
   )
