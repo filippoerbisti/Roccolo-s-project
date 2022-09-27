5f2a6e3e58623a1ff431f8dc0ba470a4366065fc
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-27 09:11:16 +0200

    manage mex if not in authorizedDates

--- mapapp/components/NoAuthPeriod.jsx
+++ mapapp/components/NoAuthPeriod.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
 
-const NoAuthPeriod = () => {
-    const { user } = useAuth();
-    const { authorizedDates } = useAuth();
+const NoAuthPeriod = ({ user, authorizedDates })  => {
+
+    var start = new Date(authorizedDates.start_date.toDate()).toString();
+    var end = new Date(authorizedDates.end_date.toDate()).toString();
 
     return (
-        <div className='no-auth-period' style={{paddingTop: '150px'}}>
+        <div className='no-auth-period'>
             <h2>
-                {/* Salve, {user.email} */}
+                Salve, {user.email}
                 <br />
                 La informiamo che la licenza per il prodotto Mapapp
-                {/* sarà attiva dal {authorizedDates.start_date.toDate()}
-                al {authorizedDates.end_date.toDate()} */}
+                sarà attiva 
+                <br /> dal: 
+                <br /> {start}
+                <br /> al:
+                <br /> {end}
 
             </h2>
         </div>
