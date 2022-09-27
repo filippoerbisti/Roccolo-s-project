c1bfc652de8110363c59ab19d7b5b73fa537950e
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-27 17:48:52 +0200

    add text faq & translate

--- mapapp/components/FAQ.jsx
+++ mapapp/components/FAQ.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FaAngleUp } from 'react-icons/fa';
+import useTranslation from 'next-translate/useTranslation';
 
 import {
     Accordion,
@@ -13,53 +14,68 @@ import {
 // import 'react-accessible-accordion/dist/fancy-example.css';
 
 const FAQ = () => {
+    const { t } = useTranslation('common');
+
+    const space = ' ';
+    const phoneNumber = "+390457581077";
+    const mailTo = "info@roccolodellago.it";
     
-  return (
-    <div>
-        <Accordion allowZeroExpanded> {/* allowZeroExpanded allows closing of all accordions */}
-            <AccordionItem>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        What harsh truths do you prefer to ignore?
-                        <FaAngleUp className='arrow' />
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                    Exercitation in fugiat est ut ad ea cupidatat ut in
-                    cupidatat occaecat ut occaecat consequat est minim minim
-                    esse tempor laborum consequat esse adipisicing eu
-                    reprehenderit enim.
-                </AccordionItemPanel>
-            </AccordionItem>
-            <AccordionItem>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        Is free will real or just an illusion?
-                        <FaAngleUp className='arrow' />
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                    In ad velit in ex nostrud dolore cupidatat consectetur
-                    ea in ut nostrud velit in irure cillum tempor laboris
-                    sed adipisicing eu esse duis nulla non.
-                </AccordionItemPanel>
-            </AccordionItem>
-            <AccordionItem>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        Is free will real or just an illusion?
-                        <FaAngleUp className='arrow' />
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                    In ad velit in ex nostrud dolore cupidatat consectetur
-                    ea in ut nostrud velit in irure cillum tempor laboris
-                    sed adipisicing eu esse duis nulla non.
-                </AccordionItemPanel>
-            </AccordionItem>
-        </Accordion>
-    </div>
-  )
+    return (
+        <div>
+            <Accordion allowZeroExpanded> {/* allowZeroExpanded allows closing of all accordions */}
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+                            {t('faq1TContact')}
+                            <FaAngleUp className='arrow' />
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                        {t('faq1PAvailable')} {space}
+                        <a href='https://goo.gl/maps/Pgp5XzNNhqQoMqVa6' target='_blank' style={{textDecoration: 'underline'}}>{t('faq1PFarm')} Roccolo del Lago</a>.
+                        <br />
+                        {t('faq1PCallUs')} {space}
+                        <a href={`tel:${phoneNumber}`} style={{textDecoration: 'underline'}}>0457581077</a> 
+                        {space} {t('faq1PMailUs')} {space}
+                        <a href={`mailto:${mailTo}`} target='_blank' rel="noopener noreferrer" style={{textDecoration: 'underline'}}>{mailTo}</a>
+                    </AccordionItemPanel>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+                            {t('faq2TDownload')}
+                            <FaAngleUp className='arrow' />
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                        {t('faq2PDownload')}
+                    </AccordionItemPanel>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+                            {t('faq2TDownload')}
+                            <FaAngleUp className='arrow' />
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                        {t('faq2PDownload')}
+                    </AccordionItemPanel>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+                            {t('faq2TDownload')}
+                            <FaAngleUp className='arrow' />
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                        {t('faq2PDownload')}
+                    </AccordionItemPanel>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    )
 }
 
 export default FAQ
