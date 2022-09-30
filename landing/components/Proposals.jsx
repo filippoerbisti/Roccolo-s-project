199d67eafe70010da8a532fcba68b800298a339f
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-30 12:39:46 +0200

    add clickable pagination

--- landing/components/Proposals.jsx
+++ landing/components/Proposals.jsx
@@ -12,7 +12,7 @@ import { Pagination } from "swiper";
 const Proposals = () => {
   return (
     <>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+      <Swiper pagination={{clickable: true}} modules={[Pagination]} className="mySwiper">
         <SwiperSlide className="proposal parallax-proposal-1">
             <h1 className='text-2xl my-5 font-bold'>Pacchetto ragazzi</h1>
             <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png" alt="" />
