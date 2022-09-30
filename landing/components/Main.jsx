845da665dca5d71d64422906bb0bcab3fb8ee4ec
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-30 11:41:14 +0200

    add data & modify section

--- landing/components/Main.jsx
+++ landing/components/Main.jsx
@@ -17,69 +17,97 @@ const Main = () => {
 
     return (
         <div>
+            {/* Intro Img in backgroud */}
             <section className="section-background parallax">
                 <div>
                     {/* EXAMPLE WITH TAILWINDCSS */}
-                    <h1 className="text-3xl font-bold text-center">
+                    <h1 className="intro-title">
                         Benvenuti al Roccolo,
                         <br />
                         dove il tempo è il signore del vino
                     </h1>
                 </div>
             </section>
-            <section className="text-bloc flex flex-row">
-                <div className='box-intro'>
-                    <h2 className='text-center text-2xl'>La storia dell'antico Roccolo</h2>
-                    <div className='flex flex-row'>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis temporibus totam odit, magni rem eaque, quas maiores voluptatum doloribus repellendus qui ipsam est error quod nobis atque dolore, non ipsa.</p>
-                        <img src='https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png' />
+
+            {/* Box history & vineyards (txt + img) */}
+            <section className="box-container">
+                <div className='mr-2.5'>
+                    <h2 className='text-center text-2xl my-5 font-bold'>La storia dell'antico Roccolo</h2>
+                    <div className='box-intro'>
+                        <div className='box-txt-img'>
+                            <div className='box-txt'>
+                                <p>Costruito nella metà dell'800, il Roccolo è una piccola costruzione di tre piani 
+                                    utilizzato in origine dai cacciatori per catturare con le reti gli  uccelli migratori.
+                                </p>
+                                <p>Tale tecnica di caccia era definita Acupio, dal latino Yaucupium, avis (uccello) e capere (prendere).
+                                    Sulle piante venivano posizionate le gabbiette con uccelli da richiamo.
+                                    Le reti erano sistemate sulla fila esterna delle piante del boschetto.
+                                </p>
+                                <p>La cattura degli uccelli era favorita dal richiamo proveniente dalle gabbiette che attiravano i volatili migratori
+                                    e da uno speciale strumento usato dai cacciatori, detto spauracchio, che veniva lanciato provocando rumore nel boschetto.
+                                    L'allarme provocato dallo spauracchio spingeva gli uccelli verso le reti di cattura.
+                                </p>
+                            </div>
+                            <img className='box-img' src='https://res.cloudinary.com/dl38nyo08/image/upload/v1664462336/mapapp/roccolo-roccolo_v1pmex.jpg' />
+                        </div>
                     </div>
                 </div>
-                <div className='box-intro'>
-                    <h2 className='text-center text-2xl'>I Vigneti</h2>
-                    <div className='flex flex-row'>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis temporibus totam odit, magni rem eaque, quas maiores voluptatum doloribus repellendus qui ipsam est error quod nobis atque dolore, non ipsa.</p>
-                        <img src='https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png' />
+                
+                <div className='ml-2.5'>
+                    <h2 className='text-center text-2xl my-5 font-bold'>I Vigneti</h2>
+                    <div className='box-intro'>
+                        <div className='box-txt-img'>
+                            <div className='box-txt'>
+                                <p>Da questi terreni selezioniamo le migliori uve per la produzione del Bardolino Classico e del Chiaretto, vini fruttatie freschi che sanno
+                                    rappresentare al meglio la produzione vitivinicola del Lago di Garda.
+                                    Corvina, Rondinella e Molinara nascono su terreni morenici di origine glaciale da vigneti che seguono un sistema di allevamento a cordone speronato libero.
+                                </p>
+                                <p>Quì, dove il Lago di Garda regola il clima del territorio, lo mitiga per garantire una temperatura media annua di 15°C, le mani esperte
+                                    seguono con amorevole cura la coltivazione della vite e si dedicano alla produzione di vini di qualità.
+                                </p>
+                            </div>
+                            <img className='box-img' src='https://res.cloudinary.com/dl38nyo08/image/upload/v1664462336/mapapp/roccolo-roccolo_v1pmex.jpg' />
+                        </div>
                     </div>
                 </div>
             </section>
 
-            <h2 className='text-center text-2xl mt-10'>Visite</h2>
-            <section className="section-background parallax1 flex flex-col">
-                <h3 className='text-2xl p-5'>Visita i posti suggestivi della tenuta e le sue cantine</h3>
-                <h4 className='text-xl p-5'>Immergiti nella natura e scopri la storia di questo posto</h4>
-                <div>
-                    <p>
-                        <span className='font-bold'>1. Azienda:</span>
+            {/* Box Visit */}
+            <h2 className='text-center text-2xl my-5 font-bold'>Visite</h2>
+            <section className="section-background parallax1 flex flex-col pb-10">
+                <h3>Visita i posti suggestivi della tenuta e le sue cantine</h3>
+                <h4>Immergiti nella natura e scopri la storia di questo posto</h4>
+                <div className='background-opacity'>
+                    <p className='py-3.5'>
+                        <span className='font-bold'>1. Azienda: </span>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo facere quod nesciunt iste quos sunt facilis soluta consectetur, modi perspiciatis obcaecati corporis delectus corrupti, temporibus repellendus? Itaque provident molestiae recusandae?
                     </p>
-                    <p>
-                        <span className='font-bold'>2. Vigneto:</span>
+                    <p className='py-3.5'>
+                        <span className='font-bold'>2. Vigneto: </span>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima impedit esse tenetur totam, eligendi, perferendis nam culpa voluptatibus, ipsam consectetur sit mollitia rerum deserunt natus maxime asperiores? Iusto, modi quos.
                     </p>
-                    <p>
-                        <span className='font-bold'>3. Cantina:</span>
+                    <p className='py-3.5'>
+                        <span className='font-bold'>3. Cantina: </span>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit saepe laborum tenetur placeat cumque. Cupiditate beatae earum dignissimos adipisci officiis dolorum dolores laborum, illum quas. Sed provident quae libero!
                     </p>
                 </div>
             </section>
 
-            <h2 className='text-center text-2xl mt-10'>Degustazioni</h2>
-            <section className="section-background parallax1 flex flex-col">
+            {/* Box Selected Tastings */}
+            <h2 className='text-center text-2xl my-5 font-bold'>Degustazioni</h2>
+            <section className="section-background parallax2 flex flex-col">
                 <h3 className='text-2xl p-5'>3 proposte</h3>
             </section>
                 
-            <section className="text-bloc stepper">
-                <Form />
+            {/* Box Form */}
+            <section className='section-background parallax-form'>
+                <div className='background-opacity-form'>
+                    <h2 className='text-center text-2xl my-5 font-bold'>Prenota la tua degustazione</h2>
+                    <div className="stepper">
+                        <Form />
+                    </div>
+                </div>
             </section>
-                
-            <section className="section-background parallax2">
-                <h2>{t('ourGrape')}</h2>
-            </section>
-            <section className="section-background parallax3">
-                <h2>{t('ourVineyards')}</h2>
-            </section>
-                    
         </div>
     )
 }
