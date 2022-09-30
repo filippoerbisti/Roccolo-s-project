58e41487f3548d9805222b9bcbb117d37b563dad
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-09-30 12:33:55 +0200

    export

--- landing/components/index.js
+++ landing/components/index.js
@@ -1,5 +1,7 @@
 export {default as Layout } from './Layout';
+export {default as Navbar } from './Navbar';
 export {default as Main } from './Main';
 export {default as Footer } from './Footer';
 
+export {default as Proposals } from './Proposals';
 export {default as Form } from './Form';
