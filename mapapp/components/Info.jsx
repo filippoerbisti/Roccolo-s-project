a024a988942ce87fc74529e6d3b745189ac6c1bc
filippoerbisti <filippo.erbisti@gmail.com>
2022-10-01 20:56:17 +0200

    pass userDoc value

--- mapapp/components/Info.jsx
+++ mapapp/components/Info.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 
-const Info = ({ user, userDoc }) => {
+const Info = ({ userDoc  }) => {
   const { t } = useTranslation('common');
-
-  // Start Date authorizedDates
-  let dateBooking = userDoc.dateBooking.getUTCDate() + "/" + (userDoc.dateBooking.getUTCMonth() + 1) + "/" + userDoc.dateBooking.getUTCFullYear();
-
-  // End Date authorizedDates
-  let dateEndBooking = userDoc.dateEndAccessApp.getUTCDate() + "/" + (userDoc.dateEndAccessApp.getUTCMonth() + 1) + "/" + userDoc.dateEndAccessApp.getUTCFullYear();
 
   return (
     <div className='info'>
       <p style={{textAlign: 'center'}}>
-        {t('helloInfo')} {user.email}, 
+        {t('helloInfo')} {userDoc.name} {userDoc.surname}, 
         <br /> {t('periodInfo')}: 
-        <br /> <span style={{fontWeight: 'bold'}}>{dateBooking} - {dateEndBooking}</span>
+        <br /> <span style={{fontWeight: 'bold'}}>{userDoc.dateBooking} - {userDoc.dateEndAccessApp}</span>
       </p>
       <br />
       <p>
--- mapapp/components/NoAuthPeriod.jsx
+++ mapapp/components/NoAuthPeriod.jsx
@@ -4,26 +4,14 @@ import useTranslation from 'next-translate/useTranslation';
 const NoAuthPeriod = ({ user, userDoc })  => {
     const { t } = useTranslation('common');
 
-    // Start Date authorizedDates
-    var dayStart = new Date(userDoc.datebooking.toDate()).getUTCMonth() + 1;
-    var monthStart = new Date(userDoc.datebooking.toDate()).getUTCDate();
-    var yearStart = new Date(userDoc.datebooking.toDate()).getUTCFullYear();
-    var start = dayStart + "/" + monthStart + "/" + yearStart;
-    
-    // End Date authorizedDates
-    var dayEnd = new Date(userDoc.dateEndAccessApp.toDate()).getUTCMonth() + 1;
-    var monthEnd = new Date(userDoc.dateEndAccessApp.toDate()).getUTCDate();
-    var yearEnd = new Date(userDoc.dateEndAccessApp.toDate()).getUTCFullYear();
-    var end = dayEnd + "/" + monthEnd + "/" + yearEnd;
-
     return (
         <div className='no-auth-period'>
             <h2>
                 {t('greetings')}, {user.email}
                 <br />
                 {t('activeLicense')}
-                <br /> {t('fromDate')}: {start}
-                <br /> {t('toDate')}: {end}
+                <br /> {t('fromDate')}: {userDoc.dateBooking}
+                <br /> {t('toDate')}: {userDoc.dateEndAccessApp}
             </h2>
         </div>
     )
