850cf66d42c5504e49a52f5946c88f2a89de8b02
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-03 12:40:47 +0200

    create hamburgerMenu

---
+++ landing/components/HamburgerMenu.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import useTranslation from 'next-translate/useTranslation';
+import { IT, GB, FR, DE } from 'country-flag-icons/react/3x2';
+import { useRouter } from 'next/router';
+
+import styles from '../styles/Hamburger.module.css';
+import { useStateContext } from '../context/StateContext';
+
+const HamburgerMenu = () => {
+  const { t } = useTranslation('common');
+
+  const currentRoute = useRouter().asPath;
+  
+  const { showMenu, setShowMenu, changeIconHamburgerMenu, setChangeIconHamburgerMenu } = useStateContext();
+
+  const langEcommerceSite = useRouter().locale;
+  const ecommerceSite = 'https://roccolodellago.vercel.app';
+
+  return (
+    <div className={styles.navigation}>
+      <Link href={"/"} className='cursor-pointer'>
+        <Image 
+          src="https://res.cloudinary.com/dl38nyo08/image/upload/v1660806591/Roccolo%20del%20Lago/logo_iidjdd.png"
+          width={300} 
+          height={120} 
+          objectFit="contain"
+          className='cursor-pointer'
+          onClick={() => 
+            {setShowMenu(!showMenu) 
+            setChangeIconHamburgerMenu(!showMenu)}
+          } 
+          blurDataURL='https://res.cloudinary.com/dl38nyo08/image/upload/v1660806591/Roccolo%20del%20Lago/logo_iidjdd.png'
+          placeholder='blur'
+        />
+      </Link>
+      <ul>
+        <li>
+          <Link href='#visit'>
+            <a data-text={t('navVisit')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navVisit')}
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href='#tasting'>
+            <a data-text={t('navTasting')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navTasting')}
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href='#booking'>
+            <a data-text={t('navBooking')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navBooking')}
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href={ecommerceSite + '/' + langEcommerceSite + '/wine'}>
+            <a data-text={t('navShop')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navShop')}
+            </a>
+          </Link> 
+        </li>
+        <li>
+          <Link href={ecommerceSite + '/' + langEcommerceSite + '/wineshop'}>
+            <a data-text={t('navWineShop')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navWineShop')}
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href={ecommerceSite + '/' + langEcommerceSite + '/contact'}>
+            <a data-text={t('navContact')}
+              onClick={() => 
+                {setShowMenu(!showMenu) 
+                setChangeIconHamburgerMenu(!showMenu)}
+              } 
+            >
+              {t('navContact')}
+            </a>
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.langContainer}>
+        <Link href={currentRoute} locale="it">
+          <IT title="Italiano" className={styles.langIcons}/>
+        </Link>
+        <Link href={currentRoute} locale="en">
+          <GB title="English" className={styles.langIcons}/>
+        </Link>
+        {/* <Link href={currentRoute} locale="de">
+          <DE title="Deutsch" className={styles.langIcons}/>
+        </Link>
+        <Link href={currentRoute} locale="fr">
+          <FR title="Français" className={styles.langIcons}/>
+        </Link> */}
+      </div>
+      <p className={styles.credit}>{t('poweredBy')} <a href="https://filippoerbisti.netlify.app" target="_blank" className={styles.poweredBy}>Filippo Erbisti</a></p>
+    </div>
+  )
+}
+
+export default HamburgerMenu
