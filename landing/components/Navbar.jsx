c0c77fcc13c1e4d9f7c619b1a43f83431a890150
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-03 13:01:11 +0200

    add link

--- landing/components/Navbar.jsx
+++ landing/components/Navbar.jsx
@@ -4,38 +4,46 @@ import Image from 'next/image';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { IT, GB, FR, DE } from 'country-flag-icons/react/3x2';
+import { useStateContext } from '../context/StateContext';
 
+import { HamburgerMenu, HamburgerIcon } from './';
 import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
     const { t } = useTranslation('common');
 
     const currentRoute = useRouter().asPath;
+    const { showMenu, changeIconHamburgerMenu, setChangeIconHamburgerMenu } = useStateContext();
+
+    const langEcommerceSite = useRouter().locale;
+    const ecommerceSite = 'https://roccolodellago.vercel.app';
 
     return (
         <div className='navbar-container'>
         
-            <ul className={styles.menu}>
-                <li>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navShop')}
-                        </a>
-                    </Link>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navWineShop')}
-                        </a>
-                    </Link>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navWineShop')}
-                        </a>
-                    </Link>
-                </li>
-            </ul>
+            <div className={styles.menuContainer}>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link href='#visit'>
+                            <a >
+                                {t('navVisit')}
+                            </a>
+                        </Link>
+                        <Link href='#tasting'>
+                            <a >
+                                {t('navTasting')}
+                            </a>
+                        </Link>
+                        <Link href='#booking'>
+                            <a >
+                                {t('navBooking')}
+                            </a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+            <div className='nav-logo'>
                 <Link href={"/"} className='cursor-pointer'>
                     <Image 
                         src="https://res.cloudinary.com/dl38nyo08/image/upload/v1660806591/Roccolo%20del%20Lago/logo_iidjdd.png"
@@ -64,36 +72,38 @@ const Navbar = () => {
                         </Link> */}
                     </div>
 
-                    {/* {showMenu && <HamburgerMenu />} */}
+                    {showMenu && <HamburgerMenu />}
 
-                    {/* <div className={!showCart ? `${styles.menuHamburgerIcon}` : ''}>
-                    <HamburgerIcon onClick={() => {
-                        setChangeIconHamburgerMenu(!changeIconHamburgerMenu)
-                    }}/>
-                    </div> */}
+                    <div className={styles.menuHamburgerIcon}>
+                        <HamburgerIcon onClick={() => {
+                            setChangeIconHamburgerMenu(!changeIconHamburgerMenu)
+                        }}/>
+                    </div>
 
                 </div>
             </div>
-
-            <ul className={styles.menu}>
-                <li>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navShop')}
-                        </a>
-                    </Link>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navWineShop')}
-                        </a>
-                    </Link>
-                    <Link href='/wine'>
-                        <a >
-                            {t('navContact')}
-                        </a>
-                    </Link>
-                </li>
-            </ul>
+            
+            <div className={styles.menuContainer}>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link href={ecommerceSite + '/' + langEcommerceSite + '/wine'}>
+                            <a >
+                                {t('navShop')}
+                            </a>
+                        </Link>
+                        <Link href={ecommerceSite + '/' + langEcommerceSite + '/wineshop'}>
+                            <a >
+                                {t('navWineShop')}
+                            </a>
+                        </Link>
+                        <Link href={ecommerceSite + '/' + langEcommerceSite + '/contact'}>
+                            <a >
+                                {t('navContact')}
+                            </a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
         </div>
     )
