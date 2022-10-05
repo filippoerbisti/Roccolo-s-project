73031d45fa69ddfa431fdb440bb641c7bcd42cf2
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-05 18:03:05 +0200

    add fullname

--- mapapp/components/NoAuthPeriod.jsx
+++ mapapp/components/NoAuthPeriod.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 
-const NoAuthPeriod = ({ user, userDoc })  => {
+const NoAuthPeriod = ({ userDoc })  => {
     const { t } = useTranslation('common');
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
+    const fullname = capitalizeFirstLetter(userDoc.name) + " " + capitalizeFirstLetter(userDoc.surname);
 
     return (
         <div className='no-auth-period'>
+            <h1 style={{marginBottom: '20px', fontSize: 'large'}}>MAPAPP</h1>
             <h2>
-                {t('greetings')}, {user.email}
+                {t('greetings')} {fullname}
                 <br />
                 {t('activeLicense')}
                 <br /> {t('fromDate')}: {userDoc.dateBooking}
