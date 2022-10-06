ee753b91364c38a7711ee08b4d596d65a77dd5f0
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-06 12:33:19 +0200

    fix text & style no-auth-period

--- mapapp/components/NoAuthPeriod.jsx
+++ mapapp/components/NoAuthPeriod.jsx
@@ -10,16 +10,22 @@ const NoAuthPeriod = ({ userDoc })  => {
     
     const fullname = capitalizeFirstLetter(userDoc.name) + " " + capitalizeFirstLetter(userDoc.surname);
 
+    var today = new Date();
+
     return (
-        <div className='no-auth-period'>
-            <h1 style={{marginBottom: '20px', fontSize: 'large'}}>MAPAPP</h1>
-            <h2>
-                {t('greetings')} {fullname}
+        <div className='no-auth-period-container'>
+            <div className='no-auth-period'>
+                <h1 style={{marginBottom: '20px', fontSize: 'larger', textAlign: 'center', letterSpacing: '2px'}}>MAPAPP</h1>
+                <h2>
+                    {t('greetings')} {fullname},
+                    <br />
+                    {t('activeLicense')}:
+                    <br /> {t('fromDate')}: {userDoc.dateBooking}
+                    <br /> {t('toDate')}: {userDoc.dateEndAccessApp}
+                </h2>
                 <br />
-                {t('activeLicense')}
-                <br /> {t('fromDate')}: {userDoc.dateBooking}
-                <br /> {t('toDate')}: {userDoc.dateEndAccessApp}
-            </h2>
+                <h3 style={{fontWeight: 'bold'}}>{t('tastingRemember')} {today.getHours() < 13 ? '11.00' : '16.00'} {t('on')} {userDoc.dateBooking}.</h3>
+            </div>
         </div>
     )
 }
