134c5f4aa53997ba12edece2bb6cba69b715877d
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-06 12:08:54 +0200

    clear

--- mapapp/components/Mapping.jsx
+++ mapapp/components/Mapping.jsx
@@ -1,43 +1,16 @@
-import React, { useCallback, useRef } from "react";
-import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
-
-const IMG_URL =
-  "https://user-images.githubusercontent.com/4661784/" +
-  "56037265-88219f00-5d37-11e9-95ef-9cb24be0190e.png";
+import React from "react";
 
 const Mapping = () => {
 
-  const imgRef = useRef();
-  const onUpdate = useCallback(({ x, y, scale }) => {
-    const { current: img } = imgRef;
-
-    if (img) {
-      const value = make3dTransformValue({ x, y, scale });
-      img.style.setProperty("transform", value);
-    }
-  }, []);
-
   return (
     <>
-      {/* <QuickPinchZoom onUpdate={onUpdate}> */}
-        {/* <img ref={imgRef} src={IMG_URL} /> */}
-
-          {/* <div id="wrapper" style={{position: "relative", paddingBottom: "74.9791492910759%", paddingTop: 0, "height": 0}}>
-            <iframe id="scaled-frame" frameBorder="0" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}} 
-              src="https://view.genial.ly/6320e70918827800110ca871" type="text/html" allowFullScreen={true} scrolling="yes">
-            </iframe> 
-          </div> 
-        </div> */}
-
-        <div style={{width: '100%', height: '80%'}}>
-          <div id="wrapper" style={{position: "relative", paddingBottom: "145%", paddingTop: 0}}>
-            <iframe id="scaled-frame" frameBorder="0" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}} 
-              src="https://view.genial.ly/6324997be49d130018188e97" type="text/html" allowFullScreen={false} scrolling="yes">
-            </iframe>
-          </div>
+      <div style={{width: '100%', height: '80%'}}>
+        <div id="wrapper" style={{position: "relative", paddingBottom: "145%", paddingTop: 0}}>
+          <iframe id="scaled-frame" frameBorder="0" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}} 
+            src="https://view.genial.ly/6324997be49d130018188e97" type="text/html" allowFullScreen={false} scrolling="yes">
+          </iframe>
         </div>
-        
-      {/* </QuickPinchZoom> */}
+      </div>
     </>
   )
 }
