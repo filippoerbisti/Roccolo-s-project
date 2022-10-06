60cc25991c7cd674aad1e234826d271cf3f139c8
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-06 12:43:58 +0200

    add imgBackground link for fakedata

--- mapapp/store/dataFakePath.js
+++ mapapp/store/dataFakePath.js
@@ -4,6 +4,7 @@ const dataFakePath = [
         path: 'path1',
         title: 'Punto Iniziale',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038624/mapapp/icone%20mapapp/inizio_jpnutc.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-434311_dbuhns.jpg',
         description: 'Punto iniziale - Start',
     },
     { 
@@ -11,6 +12,7 @@ const dataFakePath = [
         path: 'path2',
         title: 'Roccolo del Lago',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038625/mapapp/icone%20mapapp/roccolo_eyy28v.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394254/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-290316_eqrapv.jpg',
         description: 'Roccolo: dove i cacciatori si riunivano',
     },
     { 
@@ -18,6 +20,7 @@ const dataFakePath = [
         path: 'path3',
         title: 'Panoramica su vigneti',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038625/mapapp/icone%20mapapp/vigneti_a5v7rc.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394258/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-39511_nkcwju.jpg',
         description: 'Ammira i nostri vigneti',
     },
     { 
@@ -25,6 +28,7 @@ const dataFakePath = [
         path: 'path4',
         title: 'Laghetto Naturale',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038624/mapapp/icone%20mapapp/laghetto_tr2gfd.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394250/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-45209_p8m3dj.jpg',
         description: 'Lago naturale con fauna locale',
     },
     { 
@@ -32,6 +36,7 @@ const dataFakePath = [
         path: 'path5',
         title: 'Olivo secolare',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038624/mapapp/icone%20mapapp/olivo_twwyrd.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394247/Roccolo%20del%20Lago/casual%20img/pexels-elevate-1267359_ak4tur.jpg',
         description: 'Bellissimo olivo antico',
     },
     { 
@@ -39,6 +44,7 @@ const dataFakePath = [
         path: 'path6',
         title: 'Cantina vinicola',
         img: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665038624/mapapp/icone%20mapapp/cantina_ldtroh.png', 
+        imgBackground: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-434311_dbuhns.jpg',
         description: 'Assaggia i nostri vini, compra',
     }
 
