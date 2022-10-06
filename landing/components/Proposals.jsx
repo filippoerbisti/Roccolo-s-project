e2dcce8c4cdcfed28d7640fda046b97af39a1a3e
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-06 11:03:41 +0200

    fix proposal background e style

--- landing/components/Proposals.jsx
+++ landing/components/Proposals.jsx
@@ -15,18 +15,18 @@ const Proposals = () => {
 
   return (
     <>
-      <Swiper pagination={{clickable: true}} modules={[Pagination]} className="mySwiper">
+      <Swiper pagination={{clickable: true}} spaceBetween={20} modules={[Pagination]} className="mySwiper">
         <SwiperSlide className="proposal parallax-proposal-1">
-            <h1 className='text-2xl my-5 font-bold'>{t('proposal1')}</h1>
-            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png" alt="" />
+            <h1 className='text-2xl my-5 uppercase'>{t('proposal1')}</h1>
+            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1665041204/Landing%20Roccolo/tasting_package_1_dokzq3.png" alt="" />
         </SwiperSlide>
         <SwiperSlide className="proposal parallax-proposal-2">
-            <h1 className='text-2xl my-5 font-bold'>{t('proposal2')}</h1>
-            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png" alt="" />
+            <h1 className='text-2xl my-5 uppercase'>{t('proposal2')}</h1>
+            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1665041204/Landing%20Roccolo/tasting_package_2_llmpyk.png" alt="" />
         </SwiperSlide>
         <SwiperSlide className="proposal parallax-proposal-3">
-            <h1 className='text-2xl my-5 font-bold'>{t('proposal3')}</h1>
-            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1654615348/Roccolo%20del%20Lago/degustazioni_zs5pmi.png" alt="" />
+            <h1 className='text-2xl my-5 uppercase'>{t('proposal3')}</h1>
+            <img src="https://res.cloudinary.com/dl38nyo08/image/upload/v1665041204/Landing%20Roccolo/tasting_package_3_ojd5kw.png" alt="" />
         </SwiperSlide>
       </Swiper>
     </>
