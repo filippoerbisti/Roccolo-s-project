f9c6ff6b6b177cd1e21762cd6d7fed8b8da72da7
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-07 15:39:32 +0200

    add proejct sanity key

--- mapapp/lib/client.js
+++ mapapp/lib/client.js
@@ -1,16 +1,16 @@
-// import sanityClient from '@sanity/client';
-// import imageUrlBuilder from '@sanity/image-url';
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-// // To connect to Sanity.io
-// // https://www.sanity.io/manage/personal/project/2ognk6zd
-// export const client = sanityClient({
-//     projectId: '2ognk6zd',
-//     dataset: 'production',
-//     apiVersion: '2022-03-10',
-//     useCdn: true,
-//     token: process.env.NEXT_PUBLIC_SANITY_TOKEN
-// });
+// To connect to Sanity.io
+// https://www.sanity.io/manage/personal/project/2ognk6zd
+export const client = sanityClient({
+    projectId: '3rafbmzi',
+    dataset: 'production',
+    apiVersion: '2022-10-07',
+    useCdn: true,
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+});
 
-// const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
-// export const urlFor = (source) => builder.image(source);
+export const urlFor = (source) => builder.image(source);
