dfd567f8130d684303aff6aa6bd7d948b040dbf4
filippoerbisti <filippo.erbisti@gmail.com>
2022-10-09 14:19:05 +0200

    comment router.push

--- mapapp/components/Main.jsx
+++ mapapp/components/Main.jsx
@@ -115,7 +115,7 @@ const Main = ({ user, userDoc, stages }) => {
           } 
 
           let pageId = target.attributes[1].value; //get data-where of li to then compared
-          router.push('#' + pageId);
+          // router.push('#' + pageId);
 
           const divsMain = document.getElementsByClassName("content");
           // show section (id) or hidden
@@ -154,7 +154,7 @@ const Main = ({ user, userDoc, stages }) => {
     // change background color 
     document.body.style.backgroundColor = "#EDCFBC";
     const pageId = 'home'
-    router.push('#' + pageId);
+    // router.push('#' + pageId);
     const divsMain = document.getElementsByClassName("content");
     // show section (id) or hidden
     for(let i = 0; i < divsMain.length; i++) {
@@ -190,7 +190,7 @@ const Main = ({ user, userDoc, stages }) => {
     // change background color 
     document.body.style.backgroundColor = "#C4D7BD";
     const pageId = 'map';
-    router.push('#' + pageId);
+    // router.push('#' + pageId);
     const divsMain = document.getElementsByClassName("content");
     // show section (id) or hidden
     for(let i = 0; i < divsMain.length; i++) {
@@ -226,7 +226,7 @@ const Main = ({ user, userDoc, stages }) => {
     // change background color 
     document.body.style.backgroundColor = "#D6C6DD";
     const pageId = 'help';
-    router.push('#' + pageId);
+    // router.push('#' + pageId);
     const divsMain = document.getElementsByClassName("content");
     // show section (id) or hidden
     for(let i = 0; i < divsMain.length; i++) {
