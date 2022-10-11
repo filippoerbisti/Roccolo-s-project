ddb17847988c5100553576e4ef5acfed469651e5
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-11 09:24:06 +0200

    tour

--- ecommerce/pages/tour.js
+++ ecommerce/pages/tour.js
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import useTranslation from 'next-translate/useTranslation';
 
 import View360 from '../components/View360';
+import View3602 from '../components/View3602';
 import styles from '../styles/Tour.module.css'
 
 const Tour = () => {
@@ -31,20 +32,20 @@ const Tour = () => {
                     <p className={styles.paragraph}>{t('paragraphFarm')}</p>
                     <View360 className={styles.main360vr} />
                 </div>
-                {/* <div className={styles.colmx20}>
+                <div className={styles.colmx20}>
                     <h2 className={styles.title2}>
                         <Link href="/event">{t('titleFarm')}</Link>
                     </h2>
                     <p className={styles.paragraph}>{t('paragraphFarm')}</p>
-                    <iframe className={styles.main360vr} height="350px" width="75%" allowFullScreen={true} src="https://momento360.com/e/u/f18b7f26bf2a4fc2a05071421fa0e0f6?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium"></iframe>
-                </div> */}
-                <div className={styles.colmx20}>
+                    <View3602 className={styles.main360vr} />
+                </div>
+                {/* <div className={styles.colmx20}>
                     <h2 className={styles.title2}>
                         <Link href="/wineshop">{t('titleWineshop')}</Link>
                     </h2>
                     <p className={styles.paragraph}>{t('paragraphWineshop')}</p>
                     <iframe className={styles.main360vr} height="350px" width="75%" allowFullScreen={true} src="https://momento360.com/e/u/bc36ee19f7ed453f90407a518222b25b?utm_campaign=embed&utm_source=other&heading=21.77&pitch=2.3&field-of-view=67&size=medium"></iframe>
-                </div>
+                </div> */}
             </div>
         </div>
     )
