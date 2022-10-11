f23383855eba3e51d777395b185bf7c5de3dc3be
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-10-11 09:32:37 +0200

    update img

--- ecommerce/store/dataGallery.js
+++ ecommerce/store/dataGallery.js
@@ -1,18 +1,14 @@
 const dataGallery = [
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394263/Roccolo%20del%20Lago/casual%20img/pexels-brett-sayles-1374552_dobpxq.jpg', height: 250 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-ray-piedra-1545529_tv7i7l.jpg', height: 350 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394258/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-39511_nkcwju.jpg', height: 200 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-grape-things-2954924_j9cx6c.jpg', height: 300 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394255/Roccolo%20del%20Lago/casual%20img/pexels-lukas-1410138_qqs5bs.jpg', height: 350 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394256/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-434311_dbuhns.jpg', height: 250 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394254/Roccolo%20del%20Lago/casual%20img/pexels-markus-spiske-121191_winmmo.jpg', height: 350 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394252/Roccolo%20del%20Lago/casual%20img/pexels-timur-saglambilek-66636_d3jora.jpg', height: 300 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394252/Roccolo%20del%20Lago/casual%20img/pexels-bruno-cantu%C3%A1ria-774455_c92vci.jpg', height: 250 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394252/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-33265_mlqwe1.jpg', height: 400 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394250/Roccolo%20del%20Lago/casual%20img/pexels-pixabay-51947_iuzicl.jpg', height: 300 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394250/Roccolo%20del%20Lago/casual%20img/pexels-tim-mossholder-2339180_arngod.jpg', height: 250 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394250/Roccolo%20del%20Lago/casual%20img/pexels-valeria-boltneva-1123260_ky9dxc.jpg', height: 450 },
-    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1655394248/Roccolo%20del%20Lago/casual%20img/pexels-elina-sazonova-1841591_z7gstl.jpg', height: 400 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080686/Roccolo%20del%20Lago/IMG_3397_ysutbw.jpg', height: 250 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080681/Roccolo%20del%20Lago/IMG_5891_ba0qkr.jpg', height: 350 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080680/Roccolo%20del%20Lago/IMG_3380_gnhikq.jpg', height: 400 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080677/Roccolo%20del%20Lago/IMG_5581_b8pfpt.jpg', height: 300 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080679/Roccolo%20del%20Lago/IMG_3441_k6jml2.jpg', height: 350 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080666/Roccolo%20del%20Lago/IMG_3431_rwdfju.jpg', height: 250 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080666/Roccolo%20del%20Lago/IMG_3468_catyi3.jpg', height: 450 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080664/Roccolo%20del%20Lago/IMG_3437_w0fimv.jpg', height: 300 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080667/Roccolo%20del%20Lago/IMG_5233_up0agp.jpg', height: 250 },
+    { css: 'https://res.cloudinary.com/dl38nyo08/image/upload/v1665080670/Roccolo%20del%20Lago/IMG_5240_r8v4bc.jpg', height: 400 },
   ];
 
   export default dataGallery
